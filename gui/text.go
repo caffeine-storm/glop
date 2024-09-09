33e44f57a401fa45c2dc0bf2a1e3a708b11b9339
Thomas McKee <caffeine.storm@gmail.com>
2024-09-09 09:42:16 -0400

    fix: un-revert MakeDictionary and other font/text changes

--- gui/text.go
+++ gui/text.go
@@ -1,16 +1,22 @@
 package gui
 
 import (
-  "encoding/gob"
-  "image"
-  "io"
-  "math"
-  "runtime"
-  "sync"
-  "unsafe"
+	"encoding/gob"
+	"image"
+	"image/color"
+	"image/draw"
+	"io"
+	"math"
+	"runtime"
+	"sort"
+	"sync"
+	"unsafe"
 
-  "github.com/go-gl-legacy/gl"
-  "github.com/runningwild/glop/render"
+	"code.google.com/p/freetype-go/freetype"
+	"code.google.com/p/freetype-go/freetype/raster"
+	"code.google.com/p/freetype-go/freetype/truetype"
+	"github.com/go-gl-legacy/gl"
+	"github.com/runningwild/glop/render"
 )
 
 // Shader stuff - The font stuff requires that we use some simple shaders
@@ -227,6 +233,7 @@ func (d *Dictionary) RenderString(s string, x, y, z, height float64, just Justif
     x_pos = 0
     var prev rune
     for _, r := range s {
+      // TODO(tmckee): why toss out the mapped value, then look it up again?!
       if _, ok := d.data.Kerning[prev]; ok {
         x_pos += float32(d.data.Kerning[prev][r])
       }
@@ -279,7 +286,19 @@ func (d *Dictionary) RenderString(s string, x, y, z, height float64, just Justif
     d.strs[s] = strbuf
   }
 
-  render.EnableShader("glop.font")
+  // Reset x-pos
+  x_pos = float32(x)
+  switch just {
+  case Center:
+    x_pos -= width / 2
+  case Right:
+    x_pos -= width
+  }
+
+  err := render.EnableShader("glop.font")
+  if err != nil {
+    panic(err)
+  }
   defer render.EnableShader("")
 
   diff := 20/math.Pow(height, 1.0) + 5*math.Pow(d.data.Scale, 1.0)/math.Pow(height, 1.0)
@@ -324,9 +343,270 @@ func (d *Dictionary) RenderString(s string, x, y, z, height float64, just Justif
 
 }
 
+type subImage struct {
+  im     image.Image
+  bounds image.Rectangle
+}
+type transparent struct{}
+
+func (t transparent) RGBA() (r, g, b, a uint32) {
+  return 0, 0, 0, 0
+}
+func (si *subImage) ColorModel() color.Model {
+  return si.im.ColorModel()
+}
+func (si *subImage) Bounds() image.Rectangle {
+  return si.bounds
+}
+func (si *subImage) At(x, y int) color.Color {
+  b := si.bounds
+  if (image.Point{x, y}).In(b) {
+    return si.im.At(x, y)
+  }
+  return transparent{}
+}
+
+// Returns a sub-image of the input image. The bounding rectangle is the
+// smallest possible rectangle that includes all pixels that have alpha > 0,
+// with one pixel of border on all sides.
+func minimalSubImage(src image.Image) *subImage {
+  bounds := src.Bounds()
+  var new_bounds image.Rectangle
+  new_bounds.Max = bounds.Min
+  new_bounds.Min = bounds.Max
+  for x := bounds.Min.X; x <= bounds.Max.X; x++ {
+    for y := bounds.Min.Y; y <= bounds.Max.Y; y++ {
+      c := src.At(x, y)
+      _, _, _, a := c.RGBA()
+      if a > 0 {
+        if x < new_bounds.Min.X {
+          new_bounds.Min.X = x
+        }
+        if y < new_bounds.Min.Y {
+          new_bounds.Min.Y = y
+        }
+        if x > new_bounds.Max.X {
+          new_bounds.Max.X = x
+        }
+        if y > new_bounds.Max.Y {
+          new_bounds.Max.Y = y
+        }
+      }
+    }
+  }
+
+  // // We want one row/col of boundary between characters so that we don't get
+  // // annoying artifacts
+  new_bounds.Min.X--
+  new_bounds.Min.Y--
+  new_bounds.Max.X++
+  new_bounds.Max.Y++
+
+  if new_bounds.Min.X > new_bounds.Max.X || new_bounds.Min.Y > new_bounds.Max.Y {
+    new_bounds = image.Rect(0, 0, 0, 0)
+  }
+
+  return &subImage{src, new_bounds}
+}
+
+// This stupid thing is just so that our idiot-packedImage can answer queries
+// faster.  If we're going to query every pixel then it makes sense to check
+// the largest rectangles first, since they will be the correct response more
+// often than the smaller rectangles.
+type packedImageSortByArea struct {
+	*packedImage
+}
+
+func (p *packedImageSortByArea) Len() int {
+	return len(p.ims)
+}
+func (p *packedImageSortByArea) Less(i, j int) bool {
+	ai := p.ims[i].Bounds().Dx() * p.ims[i].Bounds().Dy()
+	aj := p.ims[j].Bounds().Dx() * p.ims[j].Bounds().Dy()
+	return ai > aj
+}
+func (p *packedImageSortByArea) Swap(i, j int) {
+	p.ims[i], p.ims[j] = p.ims[j], p.ims[i]
+	p.off[i], p.off[j] = p.off[j], p.off[i]
+}
+
+type packedImage struct {
+	ims    []image.Image
+	off    []image.Point
+	bounds image.Rectangle
+}
+
+func (p *packedImage) Len() int {
+	return len(p.ims)
+}
+func (p *packedImage) Less(i, j int) bool {
+	return p.ims[i].Bounds().Dy() < p.ims[j].Bounds().Dy()
+}
+func (p *packedImage) Swap(i, j int) {
+	p.ims[i], p.ims[j] = p.ims[j], p.ims[i]
+	p.off[i], p.off[j] = p.off[j], p.off[i]
+}
+func (p *packedImage) GetRect(im image.Image) image.Rectangle {
+	for i := range p.ims {
+		if im == p.ims[i] {
+			return p.ims[i].Bounds().Add(p.off[i])
+		}
+	}
+	return image.Rectangle{}
+}
+func (p *packedImage) ColorModel() color.Model {
+	return p.ims[0].ColorModel()
+}
+func (p *packedImage) Bounds() image.Rectangle {
+	return p.bounds
+}
+func (p *packedImage) At(x, y int) color.Color {
+	point := image.Point{x, y}
+	for i := range p.ims {
+		if point.In(p.ims[i].Bounds().Add(p.off[i])) {
+			return p.ims[i].At(x-p.off[i].X, y-p.off[i].Y)
+		}
+	}
+	return transparent{}
+}
+
+func packImages(ims []image.Image) *packedImage {
+	var p packedImage
+	if len(ims) == 0 {
+		panic("Cannot pack zero images")
+	}
+	p.ims = ims
+	p.off = make([]image.Point, len(p.ims))
+	sort.Sort(&p)
+
+	run := 0
+	height := 0
+	max_width := 512
+	max_height := 0
+	for i := 1; i < len(p.off); i++ {
+		run += p.ims[i-1].Bounds().Dx()
+		if run+p.ims[i].Bounds().Dx() > max_width {
+			run = 0
+			height += max_height
+			max_height = 0
+		}
+		if p.ims[i].Bounds().Dy() > max_height {
+			max_height = p.ims[i].Bounds().Dy()
+		}
+		p.off[i].X = run
+		p.off[i].Y = height
+	}
+	for i := range p.ims {
+		p.off[i] = p.off[i].Sub(p.ims[i].Bounds().Min)
+	}
+
+	// Done packing - now figure out the resulting bounds
+	p.bounds.Min.X = 1e9 // if we exceed this something else will break first
+	p.bounds.Min.Y = 1e9
+	p.bounds.Max.X = -1e9
+	p.bounds.Max.Y = -1e9
+	for i := range p.ims {
+		b := p.ims[i].Bounds()
+		min := b.Add(p.off[i]).Min
+		max := b.Add(p.off[i]).Max
+		if min.X < p.bounds.Min.X {
+			p.bounds.Min.X = min.X
+		}
+		if min.Y < p.bounds.Min.Y {
+			p.bounds.Min.Y = min.Y
+		}
+		if max.X > p.bounds.Max.X {
+			p.bounds.Max.X = max.X
+		}
+		if max.Y > p.bounds.Max.Y {
+			p.bounds.Max.Y = max.Y
+		}
+	}
+
+	sort.Sort(&packedImageSortByArea{&p})
+
+	return &p
+}
+
+func MakeDictionary(font *truetype.Font, size int) *Dictionary {
+  alphabet := " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*([]{};:'\",.<>/?\\|`~-_=+"
+  context := freetype.NewContext()
+  context.SetFont(font)
+  width := 300
+  height := 300
+  context.SetSrc(image.White)
+  dpi := 150.0
+  context.SetFontSize(float64(size))
+  context.SetDPI(dpi)
+  var letters []image.Image
+  rune_mapping := make(map[rune]image.Image)
+  rune_info := make(map[rune]runeInfo)
+  for _, r := range alphabet {
+    canvas := image.NewRGBA(image.Rect(-width/2, -height/2, width/2, height/2))
+    context.SetDst(canvas)
+    context.SetClip(canvas.Bounds())
+
+    advance, _ := context.DrawString(string([]rune{r}), raster.Point{})
+    sub := minimalSubImage(canvas)
+    letters = append(letters, sub)
+    rune_mapping[r] = sub
+    adv_x := float64(advance.X) / 256.0
+    rune_info[r] = runeInfo{Bounds: sub.bounds, Advance: adv_x}
+  }
+  packed := packImages(letters)
+
+  for _, r := range alphabet {
+    ri := rune_info[r]
+    ri.Pos = packed.GetRect(rune_mapping[r])
+    rune_info[r] = ri
+  }
+
+  dx := 1
+  for dx < packed.Bounds().Dx() {
+    dx = dx << 1
+  }
+  dy := 1
+  for dy < packed.Bounds().Dy() {
+    dy = dy << 1
+  }
+
+  pim := image.NewRGBA(image.Rect(0, 0, dx, dy))
+  draw.Draw(pim, pim.Bounds(), packed, image.Point{}, draw.Src)
+  var dict Dictionary
+  dict.data.Pix = pim.Pix
+  dict.data.Dx = pim.Bounds().Dx()
+  dict.data.Dy = pim.Bounds().Dy()
+  dict.data.Info = rune_info
+
+  dict.data.Ascii_info = make([]runeInfo, 256)
+  for r := rune(0); r < 256; r++ {
+    if info, ok := dict.data.Info[r]; ok {
+      dict.data.Ascii_info[r] = info
+    }
+  }
+  dict.data.Baseline = dict.data.Info['.'].Bounds.Min.Y
+
+  dict.data.Miny = int(1e9)
+  dict.data.Maxy = int(-1e9)
+  for _, info := range dict.data.Info {
+    if info.Bounds.Min.Y < dict.data.Miny {
+      dict.data.Miny = info.Bounds.Min.Y
+    }
+    if info.Bounds.Max.Y > dict.data.Maxy {
+      dict.data.Maxy = info.Bounds.Max.Y
+    }
+  }
+
+  dict.setupGlStuff()
+
+  return &dict
+}
+
 var init_once sync.Once
 
 func LoadDictionary(r io.Reader) (*Dictionary, error) {
+  // TODO(tmckee): we shouldn't coulple loading a dictionary to registering
+  // shaders.
   init_once.Do(func() {
     render.Queue(func() {
       err := render.RegisterShader("glop.font", []byte(font_vertex_shader), []byte(font_fragment_shader))
@@ -344,6 +624,10 @@ func LoadDictionary(r io.Reader) (*Dictionary, error) {
   }
   d.setupGlStuff()
   return &d, nil
+}
+
+func (d *Dictionary) Store(outputStream io.Writer) error {
+  return gob.NewEncoder(outputStream).Encode(d.data)
 }
 
 // Sets up anything that wouldn't have been loaded from disk, including
