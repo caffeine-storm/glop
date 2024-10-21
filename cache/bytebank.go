23cf52a7910060db160b88f9824c64d73af4ef8d
Thomas McKee <caffeine.storm@gmail.com>
2024-10-21 13:14:22 -0400

    feat: make RamByteBank implement ByteBank
    tdd: green

--- cache/bytebank.go
+++ cache/bytebank.go
@@ -32,3 +32,11 @@ func (*FsByteBank) Write(filename string, data []byte) error {
 }
 
 type RamByteBank struct{}
+
+func (*RamByteBank) Read(key string) ([]byte, bool, error) {
+	return []byte{}, true, nil
+}
+
+func (*RamByteBank) Write(key string, data []byte) error {
+	return nil
+}
