3f9867618247eb850f5609902d1fd713baace430
Thomas McKee <caffeine.storm@gmail.com>
2024-10-21 14:37:48 -0400

    feat: centralize making 'op' values for ergonomics
    tdd: refactor

--- cache/cachetest/spy.go
+++ cache/cachetest/spy.go
@@ -14,12 +14,19 @@ type SpyedCache struct {
 var _ cache.ByteBank = (*SpyedCache)(nil)
 
 func (spy *SpyedCache) Read(key string) ([]byte, bool, error) {
-	spy.Ops = append(spy.Ops, fmt.Sprintf("read %q", key))
+	spy.Ops = append(spy.Ops, spy.ReadOp(key))
 	return spy.Impl.Read(key)
 }
 
 func (spy *SpyedCache) Write(key string, data []byte) error {
-	spy.Ops = append(spy.Ops, fmt.Sprintf("write %q", key))
+	spy.Ops = append(spy.Ops, spy.WriteOp(key))
 	return spy.Impl.Write(key, data)
 }
 
+func (*SpyedCache) ReadOp(key string) string {
+	return fmt.Sprintf("read %q", key)
+}
+
+func (*SpyedCache) WriteOp(key string) string {
+	return fmt.Sprintf("write %q", key)
+}
