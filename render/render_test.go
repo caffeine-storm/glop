fb89509987f19b22bcea60fc8669cc12c11d9234
Thomas McKee <caffeine.storm@gmail.com>
2024-10-23 10:17:25 -0400

    test: RenderQueues should expose an 'IsPurging' status
    tdd: red

---
+++ render/render_test.go
@@ -0,0 +1,18 @@
+package render_test
+
+import (
+	"testing"
+
+	"github.com/runningwild/glop/render"
+)
+
+var nop = func() {}
+
+func TestRenderQueueIsPurging(t *testing.T) {
+	t.Run("a new queue is not purging", func(t *testing.T) {
+		q := render.MakeQueue(nop)
+		if q.IsPurging() {
+			t.Fatalf("a new queue should not be purging")
+		}
+	})
+}
