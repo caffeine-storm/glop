af5f8a6b48bfb09e3d56f00d894d23e66409cfa1
Thomas McKee <caffeine.storm@gmail.com>
2024-10-25 13:27:33 -0400

    feat: implement glog logger releveling
    tdd: green

---
+++ glog/glog.go
@@ -0,0 +1,49 @@
+package glog
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"os"
+)
+
+type handlerAtLevel struct {
+	slog.Handler
+	lvl slog.Level
+}
+
+func (hdl *handlerAtLevel) Enabled(ctx context.Context, lvl slog.Level) bool {
+	return lvl >= hdl.lvl
+}
+
+func Relevel(in *slog.Logger, lvl slog.Level) *slog.Logger {
+	handler := in.Handler()
+
+	handler = &handlerAtLevel{
+		Handler: handler, lvl: lvl,
+	}
+
+	return slog.New(handler)
+}
+
+type Opts struct {
+	Output io.Writer
+	Level  slog.Leveler
+}
+
+func New(options *Opts) *slog.Logger {
+	if options == nil {
+		options = &Opts{}
+	}
+	if options.Output == nil {
+		options.Output = os.Stderr
+	}
+	if options.Level == nil {
+		options.Level = slog.LevelInfo
+	}
+
+	slogopts := &slog.HandlerOptions{
+		Level: options.Level,
+	}
+	return slog.New(slog.NewTextHandler(options.Output, slogopts))
+}
