f1290941846a635446852e0eaf259636fe836b1d
Thomas McKee <caffeine.storm@gmail.com>
2024-10-30 12:41:56 -0400

    test: migrate cache to convey
    tdd: refactor

--- cache/bytebank_test.go
+++ cache/bytebank_test.go
@@ -9,10 +9,8 @@ import (
 	"path/filepath"
 	"testing"
 
-	// TODO(tmckee): use 'convey' instead:
-	// https://github.com/smartystreets/goconvey
-	"github.com/orfjackal/gospec/src/gospec"
 	"github.com/runningwild/glop/cache"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
 func withScratchDir(op func(string)) {
@@ -57,12 +55,11 @@ func setDifference(included, excluded map[string]bool) map[string]bool {
 
 func TestCacheSpecs(t *testing.T) {
 	withScratchDir(func(tmpdir string) {
-		r := gospec.NewRunner()
-		r.AddSpec(FsByteBankSpec)
-		r.AddSpec(RamByteBankSpec)
-		r.AddNamedSpec("FsByteBank is a ByteBank", ImplementsByteBank(cache.MakeFsByteBank(tmpdir)))
-		r.AddNamedSpec("ramByteBank is a ByteBank", ImplementsByteBank(cache.MakeRamByteBank()))
-		gospec.MainGoTest(r, t)
+		Convey("FsByteBankSpec", t, FsByteBankSpec)
+		Convey("RamByteBankSpec", t, RamByteBankSpec)
+
+		Convey("FsByteBank is a ByteBank", t, ImplementsByteBank(cache.MakeFsByteBank(tmpdir)))
+		Convey("RamByteBank is a ByteBank", t, ImplementsByteBank(cache.MakeRamByteBank()))
 	})
 }
 
@@ -71,21 +68,21 @@ var (
 	someOtherData = []byte("rofl")
 )
 
-func FsByteBankSpec(c gospec.Context) {
+func FsByteBankSpec() {
 	withScratchDir(func(tmpdir string) {
-		c.Specify("An empty FsByteBank", func() {
+		Convey("An empty FsByteBank", func() {
 			bank := cache.MakeFsByteBank(tmpdir)
 
-			c.Specify("propagates file writing failures", func() {
+			Convey("propagates file writing failures", func() {
 				doesNotExistDir := "/does/not/exist/"
 				_, err := os.Stat(doesNotExistDir)
-				c.Assume(errors.Is(err, fs.ErrNotExist), gospec.IsTrue)
+				So(errors.Is(err, fs.ErrNotExist), ShouldEqual, true)
 
 				err = bank.Write(path.Join(doesNotExistDir, "foo"), someData)
-				c.Expect(err, gospec.Not(gospec.IsNil))
+				So(err, ShouldNotBeNil)
 			})
 
-			c.Specify("can write to a temp file", func() {
+			Convey("can write to a temp file", func() {
 				key := "fs-byte-bank"
 				f, err := os.CreateTemp(tmpdir, key)
 				if err != nil {
@@ -99,35 +96,35 @@ func FsByteBankSpec(c gospec.Context) {
 					panic(fmt.Errorf("couldn't write data: %w", err))
 				}
 
-				c.Specify("the data can be read back", func() {
+				Convey("the data can be read back", func() {
 					data, ok, err := bank.Read(key)
-					c.Expect(err, gospec.IsNil)
-					c.Expect(ok, gospec.IsTrue)
-					c.Expect(string(data), gospec.Equals, string(someData))
+					So(err, ShouldBeNil)
+					So(ok, ShouldBeTrue)
+					So(string(data), ShouldEqual, string(someData))
 				})
 
-				c.Specify("still misses for different key", func() {
+				Convey("still misses for different key", func() {
 					_, ok, err := bank.Read(key + "-but-miss")
-					c.Expect(err, gospec.IsNil)
-					c.Expect(ok, gospec.IsFalse)
+					So(err, ShouldBeNil)
+					So(ok, ShouldBeFalse)
 				})
 			})
 
-			c.Specify("relative paths refer to the bound directory", func() {
+			Convey("relative paths refer to the bound directory", func() {
 				filesInTempDirBefore := findFilesInDir(tmpdir)
 
 				err := bank.Write("someKey", someData)
-				c.Assume(err, gospec.IsNil)
+				So(err, ShouldBeNil)
 
 				filesInTempDirAfter := findFilesInDir(tmpdir)
 
 				delta := setDifference(filesInTempDirAfter, filesInTempDirBefore)
 
-				c.Expect(len(delta) != 0, gospec.IsTrue)
+				So(len(delta), ShouldNotEqual, 0)
 			})
 		})
 
-		c.Specify("An FsByteBank with some data", func() {
+		Convey("An FsByteBank with some data", func() {
 			bank := cache.MakeFsByteBank(tmpdir)
 			keyBase := "fs-byte-bank"
 			f, err := os.CreateTemp(tmpdir, keyBase)
@@ -142,83 +139,83 @@ func FsByteBankSpec(c gospec.Context) {
 			_, key := path.Split(f.Name())
 
 			err = bank.Write(key, someData)
-			c.Assume(err, gospec.IsNil)
+			So(err, ShouldBeNil)
 
-			c.Specify("uses a flat format/encoding", func() {
+			Convey("uses a flat format/encoding", func() {
 				fileData, err := os.ReadFile(tmpfile)
-				c.Assume(err, gospec.IsNil)
+				So(err, ShouldBeNil)
 
-				c.Expect(fileData, gospec.ContainsInOrder, someData)
+				So(fileData, ShouldResemble, someData)
 			})
 		})
 
-		c.Specify("FsByteBank construction needs a directory", func() {
-			c.Specify("a temp directory is okay", func() {
+		Convey("FsByteBank construction needs a directory", func() {
+			Convey("a temp directory is okay", func() {
 				bank := cache.MakeFsByteBank(tmpdir)
-				c.Expect(bank, gospec.Not(gospec.IsNil))
+				So(bank, ShouldNotBeNil)
 			})
-			c.Specify("a missing directory is not okay", func() {
+			Convey("a missing directory is not okay", func() {
 				bank := cache.MakeFsByteBank("/does/not/exist")
-				c.Expect(bank, gospec.IsNil)
+				So(bank, ShouldBeNil)
 			})
-			c.Specify("a regular file can't be used as a directory", func() {
+			Convey("a regular file can't be used as a directory", func() {
 				regfile, err := os.Create(path.Join(tmpdir, "foo.txt"))
-				c.Assume(err, gospec.IsNil)
+				So(err, ShouldBeNil)
 
 				bank := cache.MakeFsByteBank(regfile.Name())
-				c.Expect(bank, gospec.IsNil)
+				So(bank, ShouldBeNil)
 			})
 		})
 	})
 }
 
-func RamByteBankSpec(c gospec.Context) {
-	c.Specify("An empty RamByteBank", func() {
+func RamByteBankSpec() {
+	Convey("An empty RamByteBank", func() {
 		bank := cache.MakeRamByteBank()
 
-		c.Specify("can be constructed", func() {
-			c.Expect(bank, gospec.Not(gospec.Equals), nil)
+		Convey("can be constructed", func() {
+			So(bank, ShouldNotBeNil)
 		})
 	})
 }
 
-func ImplementsByteBank(bb cache.ByteBank) func(gospec.Context) {
-	return func(c gospec.Context) {
-		c.Specify("An empty ByteBank", func() {
-			c.Specify("can be constructed", func() {
-				c.Expect(bb, gospec.Not(gospec.Equals), nil)
+func ImplementsByteBank(bb cache.ByteBank) func() {
+	return func() {
+		Convey("An empty ByteBank", func() {
+			Convey("can be constructed", func() {
+				So(bb, ShouldNotBeNil)
 			})
 
-			c.Specify("returns a 'miss' when reading a bogus key", func() {
+			Convey("returns a 'miss' when reading a bogus key", func() {
 				_, ok, err := bb.Read("not-present")
-				c.Expect(err, gospec.IsNil)
-				c.Expect(ok, gospec.Equals, false)
+				So(err, ShouldBeNil)
+				So(ok, ShouldBeFalse)
 			})
 
 			someKey := "some-key"
 			someOtherKey := "some-other-key"
 
-			c.Specify("can write a payload", func() {
+			Convey("can write a payload", func() {
 				err := bb.Write(someKey, someData)
 				if err != nil {
 					panic(fmt.Errorf("couldn't write data: %w", err))
 				}
 
-				c.Specify("can read back the payload", func() {
+				Convey("can read back the payload", func() {
 					readData, hit, err := bb.Read(someKey)
-					c.Expect(err, gospec.Equals, nil)
-					c.Expect(hit, gospec.Equals, true)
-					c.Expect(readData, gospec.ContainsInOrder, someData)
+					So(err, ShouldBeNil)
+					So(hit, ShouldBeTrue)
+					So(readData, ShouldResemble, someData)
 				})
 
-				c.Specify("keeps payloads separate by key", func() {
+				Convey("keeps payloads separate by key", func() {
 					err := bb.Write(someOtherKey, someOtherData)
-					c.Expect(err, gospec.IsNil)
+					So(err, ShouldBeNil)
 
 					readData, hit, err := bb.Read(someKey)
-					c.Expect(err, gospec.IsNil)
-					c.Expect(hit, gospec.IsTrue)
-					c.Expect(readData, gospec.ContainsInOrder, someData)
+					So(err, ShouldBeNil)
+					So(hit, ShouldBeTrue)
+					So(readData, ShouldResemble, someData)
 				})
 			})
 		})
