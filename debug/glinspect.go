843aee48f4da554637c046af33430d5ed4b38dba
Thomas McKee <caffeine.storm@gmail.com>
2024-11-07 11:46:50 -0500

    debug: add a high-level get important gl-state debug helper
    tdd: refactor

--- debug/glinspect.go
+++ debug/glinspect.go
@@ -9,23 +9,42 @@ import (
 	"github.com/go-gl-legacy/gl"
 )
 
-// Returns a string describing the current matrix mode.
-func GetMatrixMode() string {
-	buffer := []int32{0}
-	gl.GetIntegerv(gl.MATRIX_MODE, buffer)
-	mappedSymbols := map[int32]string{
+func mappedSymbols() map[gl.GLenum]string {
+	return map[gl.GLenum]string{
 		gl.MODELVIEW:  "model-view",
 		gl.PROJECTION: "projection",
 		gl.TEXTURE:    "texture",
 		gl.COLOR:      "color",
 	}
+}
 
-	result, ok := mappedSymbols[buffer[0]]
-	if !ok {
-		panic(fmt.Errorf("couldn't gl.GetInteger(gl.MATRIX_MODE): buffer[0]: %v", buffer[0]))
-	}
+// Returns an enum denoting the current matrix mode.
+func GetMatrixMode() gl.GLenum {
+	buffer := []int32{0}
+	gl.GetIntegerv(gl.MATRIX_MODE, buffer)
+	return gl.GLenum(buffer[0])
+}
 
-	return result
+func getMatrix(paramName gl.GLenum) []float64 {
+	ret := [16]float64{}
+	gl.GetDoublev(paramName, ret[:])
+	return ret[:]
+}
+
+func GetColorMatrix() []float64 {
+	return getMatrix(gl.COLOR_MATRIX)
+}
+
+func GetModelViewMatrix() []float64 {
+	return getMatrix(gl.MODELVIEW_MATRIX)
+}
+
+func GetProjectionMatrix() []float64 {
+	return getMatrix(gl.PROJECTION_MATRIX)
+}
+
+func GetTextureMatrix() []float64 {
+	return getMatrix(gl.TEXTURE_MATRIX)
 }
 
 // Returns (x-pos, y-pos, width, height) of the current viewport.
@@ -41,6 +60,29 @@ func GetDepthRange() (float64, float64) {
 	buffer := []float64{0, 0}
 	gl.GetDoublev(gl.DEPTH_RANGE, buffer)
 	return buffer[0], buffer[1]
+}
+
+// Returns a high-level description of what the current GL state is.
+type GlState struct {
+	MatrixMode       string
+	ModelViewMatrix  []float64
+	ProjectionMatrix []float64
+	ColorMatrix      []float64
+	TextureMatrix    []float64
+}
+
+func (st *GlState) String() string {
+	return fmt.Sprintf("%+v", *st)
+}
+
+func GetGlState() *GlState {
+	return &GlState{
+		MatrixMode:       mappedSymbols()[GetMatrixMode()],
+		ModelViewMatrix:  GetModelViewMatrix(),
+		ProjectionMatrix: GetProjectionMatrix(),
+		ColorMatrix:      GetColorMatrix(),
+		TextureMatrix:    GetTextureMatrix(),
+	}
 }
 
 func LogAndClearGlErrors(logger *slog.Logger) {
