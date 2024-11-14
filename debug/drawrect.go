f8bae63d20eccadf86955d622b61a199429d2b50
Thomas McKee <caffeine.storm@gmail.com>
2024-11-14 09:33:03 -0500

    fix: use better background and respect current projection matrix
    tdd: refactor

--- debug/drawrect.go
+++ debug/drawrect.go
@@ -3,12 +3,15 @@ package debug
 import "github.com/go-gl-legacy/gl"
 
 func BlankAndDrawRectNdc(x1, y1, x2, y2 float64) {
-	gl.ClearColor(0, 0, 1, 1)
+	gl.ClearColor(0.5, 0.5, 0.5, 1)
 	gl.Clear(gl.COLOR_BUFFER_BIT)
 	DrawRectNdc(x1, y1, x2, y2)
 }
 
 func DrawRectNdc(x1, y1, x2, y2 float64) {
+	gl.MatrixMode(gl.PROJECTION_MATRIX)
+	gl.PushMatrix()
+	gl.LoadIdentity()
 	gl.Begin(gl.QUADS)
 	gl.Color3f(1, 0, 0)
 	gl.Vertex2d(x1, y1)
@@ -16,4 +19,6 @@ func DrawRectNdc(x1, y1, x2, y2 float64) {
 	gl.Vertex2d(x2, y2)
 	gl.Vertex2d(x2, y1)
 	gl.End()
+
+	gl.PopMatrix()
 }
