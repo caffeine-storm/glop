555ae8e31070657526295abd41b3c20775ee2837
Thomas McKee <caffeine.storm@gmail.com>
2024-11-19 12:22:38 -0500

    test: make sure we can construct a gui.Gui
    tdd: red

---
+++ gui/gui_test.go
@@ -0,0 +1,22 @@
+package gui_test
+
+import (
+	"testing"
+
+	"github.com/runningwild/glop/gin"
+	"github.com/runningwild/glop/gui"
+)
+
+type stubEventDispatcher struct{}
+
+func (*stubEventDispatcher) RegisterEventListener(gin.Listener) {}
+
+func TestGui(t *testing.T) {
+	t.Run("Make", func(t *testing.T) {
+		dispatch := &stubEventDispatcher{}
+		_, err := gui.Make(dispatch, gui.Dims{42, 13}, "not-a-path")
+		if err != nil {
+			t.Fatalf("couldn't gui.Make: %v", err)
+		}
+	})
+}
