96fb10fff72d2fd1eb3e9dd3bad8df2c6a5cd174
Thomas McKee <caffeine.storm@gmail.com>
2024-11-19 10:03:59 -0500

    test: check behaviour of fuzzy matching comparisons
    tdd: red

--- render/rendertest/cmp_test.go
+++ render/rendertest/cmp_test.go
@@ -22,13 +22,40 @@ func TestFileNameHelpers(t *testing.T) {
 
 func TestPixelComparisonIsFuzzy(t *testing.T) {
 	t.Run("TestShouldLookLike", func(t *testing.T) {
-		someBytes := bytes.NewBuffer([]byte("some bytes"))
-		someOtherBytes := bytes.NewBuffer([]byte("some bytes"))
+		t.Run("matching", func(t *testing.T) {
+			someBytes := bytes.NewBuffer([]byte("some bytes"))
+			someOtherBytes := bytes.NewBuffer([]byte("some bytes"))
 
-		conveyResult := rendertest.ShouldLookLike(someBytes, someOtherBytes)
-		if conveyResult != "" {
-			// empty string represents 'success' in Convey
-			t.Fatalf("two readers over the same data should look the same")
-		}
+			conveyResult := rendertest.ShouldLookLike(someBytes, someOtherBytes)
+			if conveyResult != "" {
+				// empty string represents 'success' in Convey
+				t.Fatalf("two readers over the same data should look the same")
+			}
+		})
+
+		t.Run("mismatch", func(t *testing.T) {
+			someBytes := bytes.NewBuffer([]byte("some bytes"))
+			someOtherBytes := bytes.NewBuffer([]byte("different bytes"))
+
+			conveyResult := rendertest.ShouldLookLike(someBytes, someOtherBytes)
+			if conveyResult == "" {
+				// empty string represents 'success' in Convey
+				t.Fatalf("two readers over different data should look dissimilar")
+			}
+		})
+
+		t.Run("fuzzy matching", func(t *testing.T) {
+			t.Run("within tolerance", func(t *testing.T) {
+				someBytes := bytes.NewBuffer([]byte("some bytes"))
+				someOtherBytes := bytes.NewBuffer([]byte("some bytes"))
+
+				someOtherBytes.Bytes()[0] += 1
+
+				conveyResult := rendertest.ShouldLookLike(someBytes, someOtherBytes, rendertest.Threshold(5))
+				if conveyResult != "" {
+					t.Fatalf("a tolerance of 5 should not have been exceeded")
+				}
+			})
+		})
 	})
 }
