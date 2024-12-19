f5269546731a28837a2ef81e765f63fb32ebc457
Thomas McKee <caffeine.storm@gmail.com>
2024-12-19 13:49:57 -0500

    test: referencing test data from some package should look at ./testdata inside that package
    tdd: red

---
+++ render/rendertest/testing/expectation_file_test.go
@@ -0,0 +1,15 @@
+package testing
+
+import (
+	"testing"
+
+	"github.com/runningwild/glop/render/rendertest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExpectationFilePaths(t *testing.T) {
+	t.Run("from non-local package", func(t *testing.T) {
+		result := rendertest.ExpectationFile("text/lol", "pgm", 42)
+		assert.Equal(t, "testdata/text/lol/42.pgm", result)
+	})
+}
