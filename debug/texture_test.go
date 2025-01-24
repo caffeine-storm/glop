1137dbf65e4a0d3ea570154dd11cb39adcd9eee4
Thomas McKee <caffeine.storm@gmail.com>
2025-01-24 11:43:50 -0500

    test: add a test that checks an uploaded texture matches the image we made it from
    tdd: red

--- debug/texture_test.go
+++ debug/texture_test.go
@@ -13,7 +13,9 @@ import (
 
 	"github.com/go-gl-legacy/gl"
 	"github.com/runningwild/glop/debug"
+	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/render/rendertest"
+	"github.com/runningwild/glop/system"
 )
 
 const maxuint16 = 0xffff
@@ -24,6 +26,21 @@ func foreachPixel(img image.Image, check func(x, y int, col color.Color)) {
 			check(x, y, img.At(x, y))
 		}
 	}
+}
+
+func mustLoadImage(imageFilePath string) image.Image {
+	imageFilePath = path.Join("testdata", imageFilePath)
+	file, err := os.Open(imageFilePath)
+	if err != nil {
+		panic(fmt.Errorf("couldn't open file %q: %w", imageFilePath, err))
+	}
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		panic(fmt.Errorf("couldn't image.Decode: %w", err))
+	}
+
+	return img
 }
 
 func givenATexture(imageFilePath string) gl.Texture {
@@ -93,6 +110,7 @@ func isTransparent(c color.Color) bool {
 	_, _, _, a := c.RGBA()
 	return a == 0
 }
+
 func isRed(c color.Color) bool {
 	r, g, b, a := c.RGBA()
 	if r != maxuint16 {
@@ -106,6 +124,85 @@ func isRed(c color.Color) bool {
 	}
 
 	return a == maxuint16
+}
+
+func withShaderProgs(shaders *render.ShaderBank, vertShader string, fragShader string, fn func()) {
+	err := shaders.RegisterShader("debugshaders", vertShader, fragShader)
+	if err != nil {
+		panic(fmt.Errorf("couldn't register debug shaders: %w", err))
+	}
+
+	err = shaders.EnableShader("debugshaders")
+	if err != nil {
+		panic(fmt.Errorf("couldn't enable debug shaders: %w", err))
+	}
+
+	defer func() {
+		shaders.EnableShader("")
+	}()
+	fn()
+}
+
+func drawTexturedQuad(pixelBounds image.Rectangle, tex gl.Texture, shaders *render.ShaderBank) {
+	var left, right, top, bottom int32 = 0, int32(pixelBounds.Dx()), int32(pixelBounds.Dy()), 0
+	var texleft, texright, textop, texbottom int32 = 0, 1, 1, 0
+
+	// - enable texturing shaders
+	withShaderProgs(shaders, debug_vertex_shader, debug_fragment_shader, func() {
+		// - set shader variables/inputs; we want to use the 0'th texture unit.
+		shaders.SetUniformI("debugshaders", "tex", 0)
+
+		// - define geometry
+		verts := []int32{
+			// each vertex is an (x,y) in pixelspace and a (t,s) in texture space
+			left, top, texleft, textop,
+			left, bottom, texleft, texbottom,
+			right, bottom, texright, texbottom,
+
+			left, top, texleft, textop,
+			right, bottom, texright, texbottom,
+			right, top, texright, textop,
+		}
+
+		// - upload geometry
+		vertexBuffer := gl.GenBuffer()
+		vertexBuffer.Bind(gl.ARRAY_BUFFER)
+		// stride is how many bytes per vertex
+		// 4 components * 4 bytes per component
+		stride := 16
+		gl.BufferData(gl.ARRAY_BUFFER, stride*len(verts), verts, gl.STATIC_DRAW)
+
+		// - setup rendering parameters
+		gl.Enable(gl.TEXTURE_2D)
+		tex.Bind(gl.TEXTURE_2D)
+
+		gl.EnableClientState(gl.VERTEX_ARRAY)
+		defer gl.DisableClientState(gl.VERTEX_ARRAY)
+
+		gl.VertexPointer(2, gl.INT, stride, nil)
+
+		gl.EnableClientState(gl.TEXTURE_COORD_ARRAY)
+		defer gl.DisableClientState(gl.TEXTURE_COORD_ARRAY)
+		// First texture co-ordinate is two components in; each component is 4
+		// bytes so the first texture co-ordinate is 8 bytes in.
+		gl.TexCoordPointer(2, gl.INT, stride, uintptr(8))
+
+		// - render geometry
+		indices := []uint16{0, 1, 2, 3, 4, 5}
+		gl.DrawElements(gl.TRIANGLES, len(indices), gl.UNSIGNED_SHORT, indices)
+	})
+}
+
+func mustSaveImage(img image.Image, outputPath string) {
+	f, err := os.Create(outputPath)
+	if err != nil {
+		panic(fmt.Errorf("couldn't os.Create %q: %w", outputPath, err))
+	}
+
+	err = png.Encode(f, img)
+	if err != nil {
+		panic(fmt.Errorf("coudln't png.Encode %q: %w", outputPath, err))
+	}
 }
 
 func TestTextureDebugging(t *testing.T) {
@@ -197,4 +294,59 @@ func TestTextureDebugging(t *testing.T) {
 			}
 		})
 	})
+
+	t.Run("textures should match the underlying image", func(t *testing.T) {
+		// - Load an image
+		expectedImage := mustLoadImage("checker/0.png")
+
+		bounds := expectedImage.Bounds()
+		width, height := bounds.Dx(), bounds.Dy()
+		rendertest.WithGlForTest(width, height, func(_ system.System, queue render.RenderQueueInterface) {
+			var result bool
+			var resultImage image.Image
+			queue.Queue(func(st render.RenderQueueState) {
+				// - Convert it to a texture
+				tex := givenATexture("checker/0.png")
+
+				// - Blit the texture accross the entire viewport
+				drawTexturedQuad(bounds, tex, st.Shaders())
+
+				// - Verify a screenshot matches the image.
+				resultImage = debug.ScreenShotRgba(width, height)
+
+				result = rendertest.ImagesAreWithinThreshold(expectedImage, resultImage, rendertest.Threshold(3))
+			})
+			queue.Purge()
+
+			if result != true {
+				// stash a copy of the broken image we just made
+				mustSaveImage(resultImage, rendertest.MakeRejectName("testdata/checker/0.png", ".png"))
+				t.Fatalf("image mismatch")
+			}
+		})
+	})
 }
+
+const debug_vertex_shader string = `
+  #version 120
+  varying vec3 pos;
+
+  void main() {
+    gl_Position = ftransform();
+    gl_ClipVertex = gl_ModelViewMatrix * gl_Vertex;
+    gl_FrontColor = gl_Color;
+    gl_TexCoord[0] = gl_MultiTexCoord0;
+    gl_TexCoord[1] = gl_MultiTexCoord1;
+    pos = gl_Vertex.xyz;
+  }
+`
+
+const debug_fragment_shader string = `
+  #version 120
+  uniform sampler2D tex;
+
+  void main() {
+    vec2 tpos = gl_TexCoord[0].xy;
+    gl_FragColor = texture2D(tex, tpos);
+  }
+`
