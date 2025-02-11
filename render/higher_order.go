8ff8354e27a8b6e7954139367553358b78c81558
Thomas McKee <caffeine.storm@gmail.com>
2025-02-11 11:32:37 -0500

    feat: add WithMatrixMode
    tdd: green

--- render/higher_order.go
+++ render/higher_order.go
@@ -1,1 +1,8 @@
 package render
+
+import "github.com/go-gl-legacy/gl"
+
+// TODO(tmckee): can we use the type system to prevent bad enums?
+func WithMatrixMode(mode gl.GLenum, fn func()) {
+	fn()
+}
