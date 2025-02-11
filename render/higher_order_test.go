8b28d287752a54b24cb89e880db34a6bf2d0fd7f
Thomas McKee <caffeine.storm@gmail.com>
2025-02-11 11:40:31 -0500

    test: make sure WithMatrixMode changes the matrix mode
    tdd: red

--- render/higher_order_test.go
+++ render/higher_order_test.go
@@ -1,19 +1,45 @@
 package render_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/go-gl-legacy/gl"
 	"github.com/runningwild/glop/render"
+	"github.com/runningwild/glop/render/rendertest"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWithMatrixMode(t *testing.T) {
-	foo := 42
+func getCurrentMatrixMode() gl.GLenum {
+	var matmode [1]int32
+	gl.GetIntegerv(gl.MATRIX_MODE, matmode[:])
+	return gl.GLenum(matmode[0])
+}
 
-	render.WithMatrixMode(gl.MODELVIEW, func() {
-		foo = 17
+func pickADifferentMode(someMatrixMode gl.GLenum) gl.GLenum {
+	switch someMatrixMode {
+	case gl.MODELVIEW:
+		return gl.PROJECTION
+	case gl.PROJECTION:
+		fallthrough
+	case gl.TEXTURE:
+		fallthrough
+	case gl.COLOR:
+		return gl.MODELVIEW
+	default:
+		panic(fmt.Errorf("bad matrixmode: %d", someMatrixMode))
+	}
+}
+
+func TestWithMatrixMode(t *testing.T) {
+	var firstMode, actual gl.GLenum
+	rendertest.WithGl(func() {
+		firstMode = getCurrentMatrixMode()
+		secondMode := pickADifferentMode(firstMode)
+		render.WithMatrixMode(secondMode, func() {
+			actual = getCurrentMatrixMode()
+		})
 	})
 
-	assert.Equal(t, foo, 17)
+	assert.NotEqual(t, actual, firstMode)
 }
