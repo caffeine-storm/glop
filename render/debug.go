1cf103e415f29fa007ffd7915ea2b8022faad04b
Thomas McKee <caffeine.storm@gmail.com>
2025-02-28 10:12:53 -0500

    feat: add helper for showing Mat4 instances
    tdd: not-really

---
+++ render/debug.go
@@ -0,0 +1,33 @@
+package render
+
+import (
+	"fmt"
+
+	"github.com/MobRulesGames/mathgl"
+)
+
+type Showmat mathgl.Mat4
+
+func (m Showmat) String() string {
+	elements := make([]interface{}, len(m))
+	for i, elem := range m {
+		// Shove each value into 9 characters and always put a decimal point.
+		stringified := fmt.Sprintf("%#9.8g", elem)
+
+		// Unfortunately, golang will add an _extra_ 4 characters if it needs to
+		// tack on an exponent (e.g. e+09). Let's drop the 4 least-significant
+		// digits as needed to work around it.
+		if len(stringified) > 9 {
+			stringified = stringified[0:5] + stringified[len(stringified)-4:]
+		}
+		elements[i] = stringified
+	}
+
+	lineFormat := "%s, %s, %s, %s"
+	blockFormat := fmt.Sprintf("%s\n%s\n%s\n%s", lineFormat, lineFormat, lineFormat, lineFormat)
+
+	return fmt.Sprintf(
+		blockFormat,
+		elements...,
+	)
+}
---
+++ render/debug_test.go
@@ -0,0 +1,26 @@
+package render_test
+
+import (
+	"testing"
+
+	"github.com/MobRulesGames/mathgl"
+	"github.com/runningwild/glop/render"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShowMatFormat(t *testing.T) {
+	exampleMat := mathgl.Mat4{
+		0.0, 1.0, 2.0 / 3.0, 47,
+		0.0, 1.40129846e-45, 1.17549435e-38, 0.2,
+		1.0, 1.5, 1.75, 1.99999988e+9,
+		2.0, 16777215, 3.40282347e+38, 42.13,
+	}
+
+	result := render.Showmat(exampleMat).String()
+	expected := `0.0000000, 1.0000000, 0.6666669, 47.000000
+0.0000000, 1.401e-45, 1.175e-38, 0.2000000
+1.0000000, 1.5000000, 1.7500000, 1.999e+09
+2.0000000, 16777215., 3.402e+38, 42.130001`
+
+	assert.Equal(t, expected, result)
+}
