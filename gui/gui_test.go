2beffa452f0a1ad60011b6243bef901cfcc3b536
Thomas McKee <caffeine.storm@gmail.com>
2025-03-18 11:25:51 -0400

    test: should be able to make Gui instances that log to a given logger
    tdd: red

--- gui/gui_test.go
+++ gui/gui_test.go
@@ -1,14 +1,34 @@
 package gui_test
 
 import (
+	"bytes"
 	"testing"
 
+	"github.com/runningwild/glop/glog"
 	"github.com/runningwild/glop/gui"
 	"github.com/runningwild/glop/gui/guitest"
 )
 
+var dims = gui.Dims{13, 42}
+
 func TestGui(t *testing.T) {
 	t.Run("Make", func(t *testing.T) {
 		_ = guitest.MakeStubbedGui(gui.Dims{13, 42})
+	})
+
+	t.Run("make with logger", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		logger := glog.New(&glog.Opts{
+			Output: buffer,
+		})
+
+		val, err := gui.MakeLogged(dims, guitest.MakeStubbedEventDispatcher(), logger)
+		if err != nil {
+			t.Fatalf("got unexpected error while gui.Make'ing: %v", err)
+		}
+
+		if val == nil {
+			t.Fatalf("got no error from Make but got no gui either!")
+		}
 	})
 }
