2f7feb721a069fd04ffe7cc9f5d9c154211b2c88
Thomas McKee <caffeine.storm@gmail.com>
2025-03-18 11:54:13 -0400

    test: logging from withing a widget's Draw function should use the DrawingContext's logger
    tdd: red

--- gui/gui_test.go
+++ gui/gui_test.go
@@ -3,12 +3,22 @@ package gui_test
 import (
 	"testing"
 
-	"github.com/runningwild/glop/glog"
+	"github.com/runningwild/glop/glog/glogtest"
 	"github.com/runningwild/glop/gui"
 	"github.com/runningwild/glop/gui/guitest"
 )
 
 var dims = gui.Dims{13, 42}
+
+type loggingWidget struct{}
+
+func (*loggingWidget) Draw(dims gui.Dims, ctx gui.DrawingContext) {
+	ctx.GetLogger().Info("Draw got called")
+}
+
+func givenAWidgetThatLogs() *loggingWidget {
+	return &loggingWidget{}
+}
 
 func TestGui(t *testing.T) {
 	t.Run("Make", func(t *testing.T) {
@@ -16,15 +26,24 @@ func TestGui(t *testing.T) {
 	})
 
 	t.Run("make with logger", func(t *testing.T) {
-		logger := glog.VoidLogger()
+		bufferedLogger := glogtest.NewBufferedLogger()
 
-		val, err := gui.MakeLogged(dims, guitest.MakeStubbedEventDispatcher(), logger)
+		guiInstance, err := gui.MakeLogged(dims, guitest.MakeStubbedEventDispatcher(), bufferedLogger)
 		if err != nil {
 			t.Fatalf("got unexpected error while gui.Make'ing: %v", err)
 		}
 
-		if val == nil {
+		if guiInstance == nil {
 			t.Fatalf("got no error from Make but got no gui either!")
 		}
+		t.Run("can log from a widget's Draw()", func(t *testing.T) {
+			testWidget := givenAWidgetThatLogs()
+
+			testWidget.Draw(dims, guiInstance)
+
+			if !bufferedLogger.Contains("Draw got called") {
+				t.Fatalf("the testWidget should have logged 'Draw got called' buffer: %v", bufferedLogger.String())
+			}
+		})
 	})
 }
