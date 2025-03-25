be44b327d5fe2a00c929cbc63b557b81df047820
Thomas McKee <caffeine.storm@gmail.com>
2025-03-25 10:21:05 -0400

    clean: drop some commented out code
    tdd: refactor

--- gos/linux/include/glop.h
+++ gos/linux/include/glop.h
@@ -127,43 +127,6 @@ void GlopEnableVSync(int enable);
 // Takes a handle returned from GlopCreateWindow.
 void GlopSetGlContext(GlopWindowHandle hdl);
 
-/*
-
-//void CreateWindow(void**, void**, int, int, int, int);
-
-void GlopSwapBuffers(void*);
-
-void GlopThink();
-
-typedef struct {
-  short index;
-  short device_type;
-  float press_amt;
-  long long timestamp;
-  int cursor_x;
-  int cursor_y;
-  int num_lock;
-  int caps_lock;
-} GlopKeyEvent;
-void GlopClearKeyEvent(GlopKeyEvent* event) {
-  event->index = 0;
-  event->device_type = 0;
-  event->press_amt = 0;
-  event->timestamp = 0;
-  event->cursor_x = 0;
-  event->cursor_y = 0;
-  event->num_lock = 0;
-  event->caps_lock = 0;
-}
-
-void GlopGetInputEvents(void* _window, void** _events_ret, void* _num_events, void* _horizon);
-
-void GlopGetWindowDims(void* _window, int* x, int* y, int* dx, int* dy);
-
-void GlopEnableVSync(int);
-
-*/
-
 #ifdef __cplusplus
 } // extern "C"
 #endif
