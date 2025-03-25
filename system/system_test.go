e57a9038fd7034fb2ac1bc6be2a51eba49d15473
Thomas McKee <caffeine.storm@gmail.com>
2025-03-25 09:38:37 -0400

    test: system.System.CreateWindow ought to return a window handle
    tdd: red

---
+++ system/system_test.go
@@ -0,0 +1,58 @@
+package system_test
+
+import (
+	"testing"
+
+	"github.com/runningwild/glop/gin"
+	"github.com/runningwild/glop/system"
+)
+
+type stubSystem struct{}
+
+func (*stubSystem) Startup() {
+}
+
+func (*stubSystem) Think() int64 {
+	return 7
+}
+
+func (*stubSystem) CreateWindow(x, y, width, height int) {
+}
+
+func (*stubSystem) GetCursorPos() (x, y int) {
+	return
+}
+
+func (*stubSystem) HideCursor(bool) {
+}
+
+func (*stubSystem) GetWindowDims() (x, y, dx, dy int) {
+	return
+}
+
+func (*stubSystem) SetWindowSize(width, height int) {}
+
+func (*stubSystem) SwapBuffers() {}
+func (*stubSystem) GetInputEvents() []gin.EventGroup {
+	return nil
+}
+
+func (*stubSystem) EnableVSync(bool) {}
+
+var _ system.System = (*stubSystem)(nil)
+
+func GivenASystem() system.System {
+	return &stubSystem{}
+}
+
+func TestSystem(t *testing.T) {
+	t.Run("CreateWindow", func(t *testing.T) {
+		t.Run("returns a native id", func(t *testing.T) {
+			sys := GivenASystem()
+			windowHandle := sys.CreateWindow(0, 0, 42, 42)
+			if windowHandle == nil {
+				t.Fatalf("window handles should not be nil")
+			}
+		})
+	})
+}
