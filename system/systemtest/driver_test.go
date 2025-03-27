8aa917ede68cca914b4769880505c62640f69855
Thomas McKee <caffeine.storm@gmail.com>
2025-03-27 13:00:46 -0400

    test: codify weird todo as new test
    tdd: red

--- system/systemtest/driver_test.go
+++ system/systemtest/driver_test.go
@@ -69,11 +69,57 @@ func TestSystemtestDriver(t *testing.T) {
 
 		// Click on two points within the windows' shared bounds and process their
 		// events. Assert that each window sees only the click sent to it.
-		// TODO(tmckee): it seems clickA => clickB => procA => procB fails... it
-		// should not fail.
 		driverA.Click(4, 5)
 		driverA.ProcessFrame()
 		driverB.Click(9, 2)
+		driverB.ProcessFrame()
+
+		// Assert each window got their click.
+		expectedClickA := click{
+			x: 4,
+			y: 5,
+		}
+		expectedClickB := click{
+			x: 9,
+			y: 2,
+		}
+		clickA, found := LastClick(mouseEventsA)
+		if !found {
+			t.Error("no events found for driverA")
+		}
+		clickB, found := LastClick(mouseEventsB)
+		if !found {
+			t.Error("no events found for driverB")
+		}
+
+		if clickA != expectedClickA || clickB != expectedClickB {
+			t.Fatalf("click expectations failed: aclicks: %+v, bclicks: %+v", mouseEventsA, mouseEventsB)
+		}
+	})
+
+	t.Run("no ordering constraints between separate drivers' ProcessFrame() calls", func(t *testing.T) {
+		// Make two windows of the same size.
+		driverA, cleanA := GivenANewDriver()
+		defer cleanA()
+
+		driverB, cleanB := GivenANewDriver()
+		defer cleanB()
+
+		// Move each window to the same screen position.
+		driverA.PositionWindow(12, 17)
+		driverB.PositionWindow(12, 17)
+		driverA.ProcessFrame()
+		driverB.ProcessFrame()
+
+		mouseEventsA := WatchForMouseEvents(driverA)
+		mouseEventsB := WatchForMouseEvents(driverB)
+
+		// Click on two points within the windows' shared bounds and process their
+		// events. Assert that each window sees only the click sent to it.
+		// Do both clicks then both ProcessFrame calls for a regression test.
+		driverA.Click(4, 5)
+		driverB.Click(9, 2)
+		driverA.ProcessFrame()
 		driverB.ProcessFrame()
 
 		// Assert each window got their click.
