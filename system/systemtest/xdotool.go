1f47af635d24d6e50222bd506d7c8f99056989ec
Thomas McKee <caffeine.storm@gmail.com>
2025-04-01 10:41:13 -0400

    feat: add a xdotool helper that returns stdout
    tdd: refactor

--- system/systemtest/xdotool.go
+++ system/systemtest/xdotool.go
@@ -25,3 +25,26 @@ func xDoToolRun(xdotoolAnyArgs ...any) {
 		panic(fmt.Errorf("could not %q: %w", cmd.String(), err))
 	}
 }
+
+func xDoToolOutput(xdotoolAnyArgs ...any) string {
+	xdotoolArgs := make([]string, len(xdotoolAnyArgs))
+	for i, arg := range xdotoolAnyArgs {
+		switch v := arg.(type) {
+		case string:
+			xdotoolArgs[i] = v
+		case fmt.Stringer:
+			xdotoolArgs[i] = v.String()
+		default:
+			xdotoolArgs[i] = fmt.Sprintf("%v", arg)
+		}
+	}
+
+	cmd := exec.Command("xdotool", xdotoolArgs...)
+
+	bs, err := cmd.Output()
+	if err != nil {
+		panic(fmt.Errorf("could not %q: %w", cmd.String(), err))
+	}
+
+	return string(bs)
+}
