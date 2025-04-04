d9b72caa8132e30815cff25327c2bd005091d553
Thomas McKee <caffeine.storm@gmail.com>
2025-04-04 12:47:05 -0400

    test: make sure gin.EventGroup has a good mouse position API
    tdd: red

--- gin/event_test.go
+++ gin/event_test.go
@@ -7,11 +7,21 @@ import (
 )
 
 func TestEventGroup(t *testing.T) {
-	t.Run("event groups have x-y co-ordinates", func(t *testing.T) {
+	t.Run("event groups have optional x-y co-ordinates", func(t *testing.T) {
 		eg := gin.EventGroup{}
-		eg.X = eg.Y
-		if eg.X != eg.Y {
-			t.Fatalf("X and Y should behave like fields")
+		if eg.HasMousePosition() {
+			t.Fatalf("zero-valued groups must have non-existent co-ordinates")
+		}
+
+		eg.SetMousePosition(14, 44)
+
+		if !eg.HasMousePosition() {
+			t.Fatalf("there must be a position after setting one")
+		}
+
+		x, y := eg.GetMousePosition()
+		if x != 14 || y != 44 {
+			t.Fatalf("reading the mouse position should return whatever was set")
 		}
 	})
 }
