dff28ae9f0aab09dbb5efbb78e836fe0b425f44a
Thomas McKee <caffeine.storm@gmail.com>
2025-04-08 11:14:45 -0400

    test: support scrolling with the systemtest Driver
    tdd: red

--- system/systemtest/click_test.go
+++ system/systemtest/click_test.go
@@ -29,3 +29,38 @@ func TestE2EClickHelper(t *testing.T) {
 		}
 	})
 }
+
+// TODO: rename this file to mouse_test.go
+func TestE2EMouseWheelHelper(t *testing.T) {
+	testcases := []struct {
+		name           string
+		expectedScroll int
+	}{
+		{
+			name:           "scroll up",
+			expectedScroll: 7,
+		},
+		{
+			name:           "scroll down",
+			expectedScroll: -10,
+		},
+	}
+	for _, testcase := range testcases {
+		systemtest.WithTestWindowDriver(64, 64, func(driver systemtest.Driver) {
+			driver.Scroll(testcase.expectedScroll)
+			driver.ProcessFrame()
+
+			// Check that gin saw it.
+			allEvents := driver.GetEvents()
+			if len(allEvents) == 0 {
+				t.Fatalf("didn't see any events!")
+			}
+
+			actualScroll := driver.GetLastScroll()
+
+			if actualScroll != testcase.expectedScroll {
+				t.Fatalf("scroll amount didn't match! expected: %d actual %d", testcase.expectedScroll, actualScroll)
+			}
+		})
+	}
+}
