fc83ba75ac36e76331ca63f7eb47b322e84e023c
Thomas McKee <caffeine.storm@gmail.com>
2025-04-10 14:32:29 -0400

    test: asking a test window for its size should work
    tdd: red

--- system/systemtest/window_test.go
+++ system/systemtest/window_test.go
@@ -3,6 +3,7 @@ package systemtest_test
 import (
 	"testing"
 
+	"github.com/runningwild/glop/gui"
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/system/systemtest"
 )
@@ -10,5 +11,11 @@ import (
 func TestWindowExposesAQueue(t *testing.T) {
 	systemtest.WithTestWindow(64, 64, func(window systemtest.Window) {
 		var _ render.RenderQueueInterface = window.GetQueue()
+	})
+}
+
+func TestWindowExposesDimensions(t *testing.T) {
+	systemtest.WithTestWindow(64, 64, func(window systemtest.Window) {
+		var _ gui.Dims = window.GetDims()
 	})
 }
