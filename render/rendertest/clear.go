c5757c49cf5dc777e3ba3ade5108a1be28026a09
Thomas McKee <caffeine.storm@gmail.com>
2025-04-11 12:03:36 -0400

    feat: implement rendertest.ClearScreen
    tdd: green

---
+++ render/rendertest/clear.go
@@ -0,0 +1,13 @@
+package rendertest
+
+import "github.com/go-gl-legacy/gl"
+
+func ClearScreen() {
+	// Set the default clear values.
+	gl.ClearColor(0, 0, 0, 0)
+	gl.ClearDepth(1)
+	gl.ClearStencil(0)
+
+	// Clear all the bitplanes we care about.
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT)
+}
