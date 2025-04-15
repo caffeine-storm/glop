afc2c4a9cd3d1e66f85542abdb9fcd3bb543f601
Thomas McKee <caffeine.storm@gmail.com>
2025-04-15 10:06:28 -0400

    feat: harden DrawRectNdc against weird ModelView or Projection matrices
    tdd: refactor

--- render/rendertest/drawrect.go
+++ render/rendertest/drawrect.go
@@ -1,6 +1,9 @@
 package rendertest
 
-import "github.com/go-gl-legacy/gl"
+import (
+	"github.com/go-gl-legacy/gl"
+	"github.com/runningwild/glop/render"
+)
 
 func BlankAndDrawRectNdc(x1, y1, x2, y2 float64) {
 	gl.ClearColor(0.5, 0.5, 0.5, 1)
@@ -9,19 +12,22 @@ func BlankAndDrawRectNdc(x1, y1, x2, y2 float64) {
 }
 
 func DrawRectNdc(x1, y1, x2, y2 float64) {
-	gl.MatrixMode(gl.PROJECTION)
-	gl.PushMatrix()
-	gl.LoadIdentity()
-	gl.Begin(gl.TRIANGLES)
-	gl.Color3f(1, 0, 0)
-	gl.Vertex2d(x1, y1)
-	gl.Vertex2d(x1, y2)
-	gl.Vertex2d(x2, y2)
+	// TODO(tmckee:#32): add a nicer "use a vanilla Fixed-Function-Pipeline
+	// state" helper
+	id := &render.Matrix{}
+	id.Identity()
+	render.WithMatrixInMode(id, render.MatrixModeModelView, func() {
+		render.WithMatrixInMode(id, render.MatrixModeProjection, func() {
+			gl.Begin(gl.TRIANGLES)
+			gl.Color3f(1, 0, 0)
+			gl.Vertex2d(x1, y1)
+			gl.Vertex2d(x1, y2)
+			gl.Vertex2d(x2, y2)
 
-	gl.Vertex2d(x1, y1)
-	gl.Vertex2d(x2, y2)
-	gl.Vertex2d(x2, y1)
-	gl.End()
-
-	gl.PopMatrix()
+			gl.Vertex2d(x1, y1)
+			gl.Vertex2d(x2, y2)
+			gl.Vertex2d(x2, y1)
+			gl.End()
+		})
+	})
 }
