570ebb8ad4143b19df2e137280dc1dd9c669053c
Thomas McKee <caffeine.storm@gmail.com>
2025-05-01 13:52:48 -0400

    clean: cut down on some duplication in the tests
    tdd: refactor

--- render/rendertest/drawrect_test.go
+++ render/rendertest/drawrect_test.go
@@ -17,56 +17,47 @@ func TestDrawRect(t *testing.T) {
 	Convey("rendertest.DrawRect should work", t, DrawRectSpec)
 }
 
-func DrawRectSpec() {
-	width, height := 50, 50
-	var result *image.RGBA
+var width, height = 50, 50
 
-	Convey("with gl.TEXTURE_2D enabled", func() {
-		rendertest.WithGlForTest(width, height, func(sys system.System, queue render.RenderQueueInterface) {
-			queue.Queue(func(render.RenderQueueState) {
+func RunTest(withTex2d bool) {
+	rendertest.WithGlForTest(width, height, func(sys system.System, queue render.RenderQueueInterface) {
+		var result *image.RGBA
+		queue.Queue(func(render.RenderQueueState) {
+			if withTex2d {
 				gl.Enable(gl.TEXTURE_2D)
-				rendertest.BlankAndDrawRectNdc(-1, -1, 1, 1)
-				result = debug.ScreenShotRgba(width, height)
-			})
-			queue.Purge()
-
-			if len(result.Pix) != width*height*4 {
-				panic(fmt.Errorf("wrong number of bytes, expected %d got %d", width*height*4, len(result.Pix)))
+			} else {
+				gl.Disable(gl.TEXTURE_2D)
 			}
-
-			Convey("Should see red pixels", func() {
-				So(queue, rendertest.ShouldLookLikeFile, "red")
-			})
+			rendertest.BlankAndDrawRectNdc(-1, -1, 1, 1)
+			result = debug.ScreenShotRgba(width, height)
 		})
+		queue.Purge()
+
+		if len(result.Pix) != width*height*4 {
+			panic(fmt.Errorf("wrong number of bytes, expected %d got %d", width*height*4, len(result.Pix)))
+		}
+
+		Convey("Should see red pixels", func() {
+			So(queue, rendertest.ShouldLookLikeFile, "red")
+		})
+	})
+}
+
+func DrawRectSpec() {
+	Convey("with gl.TEXTURE_2D enabled", func() {
+		RunTest(true)
 	})
 
 	Convey("with gl.TEXTURE_2D disabled", func() {
-		rendertest.WithGlForTest(width, height, func(sys system.System, queue render.RenderQueueInterface) {
-			queue.Queue(func(render.RenderQueueState) {
-				gl.Disable(gl.TEXTURE_2D)
-				rendertest.BlankAndDrawRectNdc(-1, -1, 1, 1)
-				result = debug.ScreenShotRgba(width, height)
-			})
-			queue.Purge()
-
-			if len(result.Pix) != width*height*4 {
-				panic(fmt.Errorf("wrong number of bytes, expected %d got %d", width*height*4, len(result.Pix)))
-			}
-
-			Convey("Should see red pixels", func() {
-				// TODO(tmckee:#35): pick a different expected file or else we'd
-				// clobber things if both tests fail.
-				So(queue, rendertest.ShouldLookLikeFile, "red")
-			})
-		})
+		RunTest(false)
 	})
 }
 
 /* DANGER WILL ROBINSON! XXX: this has been crashing windows when running on
 * WSL if we don't cache XWindow instances/glxContexts. Run at your PERIL!
 * func TestDrawManyRects(t *testing.T) {
-* for i := 0; i < 500; i++ {
-*   TestDrawRect(t)
-* }
-}
+	* for i := 0; i < 500; i++ {
+		*   TestDrawRect(t)
+		* }
+	}
 */
