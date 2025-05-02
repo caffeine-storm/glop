705d04ce65104efc1bf8ccad98f0a7f7b40ce5f3
Thomas McKee <caffeine.storm@gmail.com>
2025-05-02 11:56:56 -0400

    test: add a symetric test case
    tdd: refactor

--- render/rendertest/texture_test.go
+++ render/rendertest/texture_test.go
@@ -12,13 +12,23 @@ import (
 )
 
 func TestDrawTexturedQuad(t *testing.T) {
-	Convey("enables texturing itself", t, func() {
+	Convey("doesn't care about state of gl.TEXTURE_2D", t, func() {
 		screen := image.Rect(0, 0, 64, 64)
 		subscreen := image.Rect(16, 16, 48, 48)
 		rendertest.WithGlForTest(screen.Dx(), screen.Dy(), func(sys system.System, queue render.RenderQueueInterface) {
 			queue.Queue(func(st render.RenderQueueState) {
-				tex := rendertest.GivenATexture("red/0.png")
 				gl.Disable(gl.TEXTURE_2D)
+				tex := rendertest.GivenATexture("red/0.png")
+				rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
+			})
+			queue.Purge()
+
+			So(queue, rendertest.ShouldLookLikeFile, "subred")
+		})
+		rendertest.WithGlForTest(screen.Dx(), screen.Dy(), func(sys system.System, queue render.RenderQueueInterface) {
+			queue.Queue(func(st render.RenderQueueState) {
+				gl.Enable(gl.TEXTURE_2D)
+				tex := rendertest.GivenATexture("red/0.png")
 				rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
 			})
 			queue.Purge()
