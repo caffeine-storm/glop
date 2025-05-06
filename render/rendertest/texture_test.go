8962482494fae02763e27d0443fa5c15e44b80e5
Thomas McKee <caffeine.storm@gmail.com>
2025-05-06 12:18:38 -0400

    test: drawing textured quads should work whether others leaked gl.ELEMENT_ARRAY_BUFFER bindings or not
    tdd: red

--- render/rendertest/texture_test.go
+++ render/rendertest/texture_test.go
@@ -3,6 +3,7 @@ package rendertest_test
 import (
 	"image"
 	"testing"
+	"unsafe"
 
 	"github.com/go-gl-legacy/gl"
 	"github.com/runningwild/glop/render"
@@ -10,6 +11,17 @@ import (
 	"github.com/runningwild/glop/system"
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+// TODO(tmckee:clean): copy/paste from debug/dump_buffer_test.go
+func givenABufferWithData(data []float32) gl.Buffer {
+	result := gl.GenBuffer()
+	result.Bind(gl.ARRAY_BUFFER)
+
+	floatSize := int(unsafe.Sizeof(float32(0)))
+	gl.BufferData(gl.ARRAY_BUFFER, floatSize*len(data), data, gl.STATIC_DRAW)
+
+	return result
+}
 
 func TestDrawTexturedQuad(t *testing.T) {
 	Convey("doesn't care about state of gl.TEXTURE_2D", t, func() {
@@ -35,5 +47,35 @@ func TestDrawTexturedQuad(t *testing.T) {
 
 			So(queue, rendertest.ShouldLookLikeFile, "subred")
 		})
+
+	})
+
+	Convey("doesn't care about state of gl.ELEMENT_ARRAY_BUFFER", t, func() {
+		screen := image.Rect(0, 0, 64, 64)
+		subscreen := image.Rect(16, 16, 48, 48)
+		rendertest.WithGlForTest(screen.Dx(), screen.Dy(), func(sys system.System, queue render.RenderQueueInterface) {
+			queue.Queue(func(st render.RenderQueueState) {
+				gl.Buffer(0).Bind(gl.ELEMENT_ARRAY_BUFFER)
+				tex := rendertest.GivenATexture("red/0.png")
+				rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
+			})
+			queue.Purge()
+
+			So(queue, rendertest.ShouldLookLikeFile, "subred")
+		})
+		rendertest.WithGlForTest(screen.Dx(), screen.Dy(), func(sys system.System, queue render.RenderQueueInterface) {
+			queue.Queue(func(st render.RenderQueueState) {
+				someStaleBuffer := givenABufferWithData([]float32{
+					77, 55, 44, 33, 22, 11,
+				})
+				someStaleBuffer.Bind(gl.ELEMENT_ARRAY_BUFFER)
+				tex := rendertest.GivenATexture("red/0.png")
+				rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
+			})
+			queue.Purge()
+
+			So(queue, rendertest.ShouldLookLikeFile, "subred")
+		})
+
 	})
 }
