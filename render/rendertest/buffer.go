cd2d96aacb44b143382caa3efff56e5c948ce11c
Thomas McKee <caffeine.storm@gmail.com>
2025-05-06 12:37:55 -0400

    fix: don't clobber ARRAY_BUFFER when grabbing a one-off buffer for testing
    tdd: green

--- render/rendertest/buffer.go
+++ render/rendertest/buffer.go
@@ -7,6 +7,10 @@ import (
 )
 
 func GivenABufferWithData(data []float32) gl.Buffer {
+	oldbuf := [1]int32{}
+	gl.GetIntegerv(gl.ARRAY_BUFFER_BINDING, oldbuf[:])
+	defer gl.Buffer(oldbuf[0]).Bind(gl.ARRAY_BUFFER)
+
 	result := gl.GenBuffer()
 	result.Bind(gl.ARRAY_BUFFER)
 
