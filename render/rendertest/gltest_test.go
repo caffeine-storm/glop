122e0a43e80d17bf873f2a415b4ed3b573fee747
Thomas McKee <caffeine.storm@gmail.com>
2025-05-08 10:52:14 -0400

    test: GlTest helpers ought to support sizing the viewport
    tdd: red

--- render/rendertest/gltest_test.go
+++ render/rendertest/gltest_test.go
@@ -3,6 +3,7 @@ package rendertest_test
 import (
 	"testing"
 
+	"github.com/runningwild/glop/debug"
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/render/rendertest"
 	"github.com/stretchr/testify/assert"
@@ -26,6 +27,18 @@ func TestGlTestHelpers(t *testing.T) {
 				render.MustBeOnRenderThread()
 			})
 			queue.Purge()
+		})
+	})
+
+	t.Run("can specify dimensions", func(t *testing.T) {
+		t.Run("with literal sizes", func(t *testing.T) {
+			assert := assert.New(t)
+			rendertest.GlTest().WithSize(64, 128).Run(func() {
+				render.MustBeOnRenderThread()
+				_, _, dx, dy := debug.GetViewport()
+				assert.Equal(dx, uint32(64))
+				assert.Equal(dy, uint32(128))
+			})
 		})
 	})
 }
