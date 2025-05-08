85c11ea55c6bb7f7832b6d0d2f3c6e6575830de2
Thomas McKee <caffeine.storm@gmail.com>
2025-05-08 10:38:06 -0400

    feat: support running tests that want a reference to a render queue
    tdd: green

--- render/rendertest/gltest.go
+++ render/rendertest/gltest.go
@@ -1,9 +1,25 @@
 package rendertest
 
+import (
+	"github.com/runningwild/glop/render"
+	"github.com/runningwild/glop/system"
+)
+
 type GlTestBuilder struct{}
+type queueGlTestBuilder struct{}
 
 func (b *GlTestBuilder) Run(fn func()) {
 	WithGl(fn)
+}
+
+func (b *queueGlTestBuilder) Run(fn func(render.RenderQueueInterface)) {
+	WithGlForTest(64, 64, func(_ system.System, queue render.RenderQueueInterface) {
+		fn(queue)
+	})
+}
+
+func (b *GlTestBuilder) WithQueue() *queueGlTestBuilder {
+	return &queueGlTestBuilder{}
 }
 
 func GlTest() *GlTestBuilder {
