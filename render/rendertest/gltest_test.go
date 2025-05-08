40d3f3dc7f6ff9f8611e261b0d2b5d3728cedc98
Thomas McKee <caffeine.storm@gmail.com>
2025-05-08 10:32:34 -0400

    test: ought to be able to run a test that takes a render queue as input
    tdd: red

--- render/rendertest/gltest_test.go
+++ render/rendertest/gltest_test.go
@@ -5,12 +5,27 @@ import (
 
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/render/rendertest"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGlTestHelpers(t *testing.T) {
 	t.Run("default builder runs on render thread", func(t *testing.T) {
 		rendertest.GlTest().Run(func() {
 			render.MustBeOnRenderThread()
+		})
+	})
+
+	t.Run("can run off of render thread", func(t *testing.T) {
+		assert := assert.New(t)
+		rendertest.GlTest().WithQueue().Run(func(queue render.RenderQueueInterface) {
+			assert.Panics(func() {
+				render.MustBeOnRenderThread()
+			})
+
+			queue.Queue(func(render.RenderQueueState) {
+				render.MustBeOnRenderThread()
+			})
+			queue.Purge()
 		})
 	})
 }
