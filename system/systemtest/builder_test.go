336380759cda76f2b8e4d5f427585cfff04d6174
Thomas McKee <caffeine.storm@gmail.com>
2025-05-10 10:03:00 -0400

    fix: actually, we should run systemtest.TestBuidler things off of the render thread 😅
    tdd: red

--- system/systemtest/builder_test.go
+++ system/systemtest/builder_test.go
@@ -11,7 +11,7 @@ import (
 func TestSystemtestBuilder(t *testing.T) {
 	t.Run("can wrap a rendertest builder", func(t *testing.T) {
 		systemtest.TestBuilder(testbuilder.New()).Run(func(systemtest.Window) {
-			rendertest.AssertOnRenderThread(t)
+			rendertest.AssertOffRenderThread(t)
 		})
 	})
 }
