133254d5d923dbc77e3b882a593be3e3e7afa8d4
Thomas McKee <caffeine.storm@gmail.com>
2025-05-14 11:37:40 -0400

    clean: move tests towards the code they're actually testing
    tdd: refactor

--- render/rendertest/gltest_test.go
+++ render/rendertest/gltest_test.go
@@ -2,88 +2,13 @@ package rendertest_test
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
-	"github.com/go-gl-legacy/gl"
-	"github.com/runningwild/glop/debug"
-	"github.com/runningwild/glop/gloptest"
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/render/rendertest"
 	"github.com/runningwild/glop/render/rendertest/testbuilder"
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
-
-func TestGlTestHelpers(t *testing.T) {
-	t.Run("default builder runs on render thread", func(t *testing.T) {
-		testbuilder.New().Run(func() {
-			rendertest.AssertOnRenderThread(t)
-		})
-	})
-
-	t.Run("can run off of render thread", func(t *testing.T) {
-		testbuilder.New().WithQueue().Run(func(queue render.RenderQueueInterface) {
-			rendertest.AssertOffRenderThread(t)
-
-			queue.Queue(func(render.RenderQueueState) {
-				rendertest.AssertOnRenderThread(t)
-			})
-			queue.Purge()
-		})
-	})
-
-	t.Run("can specify dimensions", func(t *testing.T) {
-		t.Run("with literal sizes", func(t *testing.T) {
-			assert := assert.New(t)
-			testbuilder.New().WithSize(64, 128).Run(func() {
-				rendertest.AssertOnRenderThread(t)
-				_, _, dx, dy := debug.GetViewport()
-				assert.Equal(dx, uint32(64))
-				assert.Equal(dy, uint32(128))
-			})
-		})
-
-		t.Run("and get the queue after", func(t *testing.T) {
-			assert := assert.New(t)
-			testbuilder.New().WithSize(64, 128).WithQueue().Run(func(queue render.RenderQueueInterface) {
-				rendertest.AssertOffRenderThread(t)
-				queue.Queue(func(render.RenderQueueState) {
-					_, _, dx, dy := debug.GetViewport()
-					assert.Equal(dx, uint32(64))
-					assert.Equal(dy, uint32(128))
-				})
-				queue.Purge()
-			})
-		})
-
-		t.Run("and get the queue first", func(t *testing.T) {
-			assert := assert.New(t)
-			testbuilder.New().WithQueue().WithSize(64, 128).Run(func(queue render.RenderQueueInterface) {
-				rendertest.AssertOffRenderThread(t)
-				queue.Queue(func(render.RenderQueueState) {
-					_, _, dx, dy := debug.GetViewport()
-					assert.Equal(dx, uint32(64))
-					assert.Equal(dy, uint32(128))
-				})
-				queue.Purge()
-			})
-		})
-	})
-}
-
-func TestGlStateLeakage(t *testing.T) {
-	t.Run("testbuilder should complain upon leakage", func(t *testing.T) {
-		assert.Panics(t, func() {
-			testbuilder.New().Run(func() {
-				// An example of tainted state is leaving ELEMENT_ARRAY_BUFFER bound
-				buf := rendertest.GivenABufferWithData([]float32{0, 1, 2, 0, 2, 3})
-				buf.Bind(gl.ELEMENT_ARRAY_BUFFER)
-			})
-		})
-	})
-}
 
 func TestFailureDoesNotCascade(t *testing.T) {
 	assert.Panics(t, func() {
@@ -110,7 +35,6 @@ func TestFailureDoesNotCascade(t *testing.T) {
 		assert := assert.New(t)
 
 		shouldGetHere := false
-		shouldNotGetHere := false
 		shouldAlsoNotGetHere := false
 
 		assert.Panics(func() {
@@ -119,7 +43,6 @@ func TestFailureDoesNotCascade(t *testing.T) {
 
 				queue.Queue(func(st render.RenderQueueState) {
 					panic(fmt.Errorf("yup; that's a panic"))
-					shouldNotGetHere = true
 				})
 				queue.Purge()
 
@@ -128,24 +51,6 @@ func TestFailureDoesNotCascade(t *testing.T) {
 		})
 
 		assert.True(shouldGetHere)
-		assert.False(shouldNotGetHere)
 		assert.False(shouldAlsoNotGetHere)
-	})
-}
-
-func TestConveyHalting(t *testing.T) {
-	t.Run("if the Convey framework wants to halt, we don't confuse things", func(t *testing.T) {
-		shouldBeFalse := false
-		output := gloptest.CollectOutput(func() {
-			Convey("but detached from a 'real' testing.T so that we don't fail the whole test", &testing.T{}, func() {
-				testbuilder.New().WithQueue().Run(func(queue render.RenderQueueInterface) {
-					So(1, ShouldNotEqual, 1)
-				})
-				shouldBeFalse = true
-			})
-		})
-
-		require.False(t, shouldBeFalse)
-		assert.NotContains(t, strings.Join(output, "\n"), "___FAILURE_HALT___")
 	})
 }
--- render/rendertest/testbuilder/builder_test.go
+++ render/rendertest/testbuilder/builder_test.go
@@ -2,13 +2,21 @@ package testbuilder_test
 
 import (
 	"log"
+	"strings"
 	"testing"
 
 	"github.com/go-gl-legacy/gl"
+	"github.com/runningwild/glop/debug"
+	"github.com/runningwild/glop/gloptest"
+	"github.com/runningwild/glop/render"
+	"github.com/runningwild/glop/render/rendertest"
 	"github.com/runningwild/glop/render/rendertest/testbuilder"
+	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestWithGl(t *testing.T) {
+func TestRunningOnRenderThread(t *testing.T) {
 	testbuilder.New().Run(func() {
 		versionString := gl.GetString(gl.VERSION)
 		log.Printf("versionString: %q\n", versionString)
@@ -16,5 +24,91 @@ func TestWithGl(t *testing.T) {
 		if versionString == "" {
 			t.Error("gl.GetString(gl.VERSION) must not return the empty string once OpenGL is initialized")
 		}
+	})
+}
+
+func TestConveyHalting(t *testing.T) {
+	t.Run("if the Convey framework wants to halt, we don't confuse things", func(t *testing.T) {
+		shouldBeFalse := false
+		output := gloptest.CollectOutput(func() {
+			Convey("but detached from a 'real' testing.T so that we don't fail the whole test", &testing.T{}, func() {
+				testbuilder.New().WithQueue().Run(func(queue render.RenderQueueInterface) {
+					So(1, ShouldNotEqual, 1)
+				})
+				shouldBeFalse = true
+			})
+		})
+
+		require.False(t, shouldBeFalse)
+		assert.NotContains(t, strings.Join(output, "\n"), "___FAILURE_HALT___")
+	})
+}
+
+func TestBuilderFluentApi(t *testing.T) {
+	t.Run("default builder runs on render thread", func(t *testing.T) {
+		testbuilder.New().Run(func() {
+			rendertest.AssertOnRenderThread(t)
+		})
+	})
+
+	t.Run("can run off of render thread", func(t *testing.T) {
+		testbuilder.New().WithQueue().Run(func(queue render.RenderQueueInterface) {
+			rendertest.AssertOffRenderThread(t)
+
+			queue.Queue(func(render.RenderQueueState) {
+				rendertest.AssertOnRenderThread(t)
+			})
+			queue.Purge()
+		})
+	})
+
+	t.Run("can specify dimensions", func(t *testing.T) {
+		t.Run("with literal sizes", func(t *testing.T) {
+			assert := assert.New(t)
+			testbuilder.New().WithSize(64, 128).Run(func() {
+				rendertest.AssertOnRenderThread(t)
+				_, _, dx, dy := debug.GetViewport()
+				assert.Equal(dx, uint32(64))
+				assert.Equal(dy, uint32(128))
+			})
+		})
+
+		t.Run("and get the queue after", func(t *testing.T) {
+			assert := assert.New(t)
+			testbuilder.New().WithSize(64, 128).WithQueue().Run(func(queue render.RenderQueueInterface) {
+				rendertest.AssertOffRenderThread(t)
+				queue.Queue(func(render.RenderQueueState) {
+					_, _, dx, dy := debug.GetViewport()
+					assert.Equal(dx, uint32(64))
+					assert.Equal(dy, uint32(128))
+				})
+				queue.Purge()
+			})
+		})
+
+		t.Run("and get the queue first", func(t *testing.T) {
+			assert := assert.New(t)
+			testbuilder.New().WithQueue().WithSize(64, 128).Run(func(queue render.RenderQueueInterface) {
+				rendertest.AssertOffRenderThread(t)
+				queue.Queue(func(render.RenderQueueState) {
+					_, _, dx, dy := debug.GetViewport()
+					assert.Equal(dx, uint32(64))
+					assert.Equal(dy, uint32(128))
+				})
+				queue.Purge()
+			})
+		})
+	})
+}
+
+func TestGlStateLeakage(t *testing.T) {
+	t.Run("testbuilder should complain upon leakage", func(t *testing.T) {
+		assert.Panics(t, func() {
+			testbuilder.New().Run(func() {
+				// An example of tainted state is leaving ELEMENT_ARRAY_BUFFER bound
+				buf := rendertest.GivenABufferWithData([]float32{0, 1, 2, 0, 2, 3})
+				buf.Bind(gl.ELEMENT_ARRAY_BUFFER)
+			})
+		})
 	})
 }
