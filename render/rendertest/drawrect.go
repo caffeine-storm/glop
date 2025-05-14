33b22bad1751e6bf4c4e85b17792b08f95b3fc0a
Thomas McKee <caffeine.storm@gmail.com>
2025-05-14 13:37:23 -0400

    refactor: use symmetry for more complete drawrect helpers
    tdd: refactor

--- render/rendertest/drawrect.go
+++ render/rendertest/drawrect.go
@@ -6,27 +6,21 @@ import (
 )
 
 func BlankAndDrawRectNdc(x1, y1, x2, y2 float64) {
+	render.WithFreshMatrices(func() {
+		BlankAndDrawRect(x1, y1, x2, y2)
+	})
+}
+
+func BlankAndDrawRect(x1, y1, x2, y2 float64) {
 	WithClearColour(0, 0, 0, 1, func() {
 		gl.Clear(gl.COLOR_BUFFER_BIT)
-		DrawRectNdc(x1, y1, x2, y2)
+		DrawRect(x1, y1, x2, y2)
 	})
 }
 
 func DrawRectNdc(x1, y1, x2, y2 float64) {
 	render.WithFreshMatrices(func() {
-		render.WithoutTexturing(func() {
-			render.WithColour(1, 0, 0, 1, func() {
-				gl.Begin(gl.TRIANGLES)
-				gl.Vertex2d(x1, y1)
-				gl.Vertex2d(x1, y2)
-				gl.Vertex2d(x2, y2)
-
-				gl.Vertex2d(x1, y1)
-				gl.Vertex2d(x2, y2)
-				gl.Vertex2d(x2, y1)
-				gl.End()
-			})
-		})
+		DrawRect(x1, y1, x2, y2)
 	})
 }
 
