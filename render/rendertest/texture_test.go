68469fc4e0e27865c333963ff652d2e067e119f0
Thomas McKee <caffeine.storm@gmail.com>
2025-05-27 12:36:23 -0400

    clean: prefer testbuilder API for rendertests
    tdd: refactor

--- render/rendertest/texture_test.go
+++ render/rendertest/texture_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/runningwild/glop/render"
 	"github.com/runningwild/glop/render/rendertest"
 	"github.com/runningwild/glop/render/rendertest/testbuilder"
-	"github.com/runningwild/glop/system"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -27,33 +26,21 @@ func TestDrawTexturedQuad(t *testing.T) {
 		})
 	})
 
-	Convey("doesn't care about state of gl.ELEMENT_ARRAY_BUFFER", t, func() {
-		screen := image.Rect(0, 0, 64, 64)
+	Convey("doesn't care about state of gl.ELEMENT_ARRAY_BUFFER", t, func(c C) {
 		subscreen := image.Rect(16, 16, 48, 48)
-		rendertest.DeprecatedWithGlForTest(screen.Dx(), screen.Dy(), func(sys system.System, queue render.RenderQueueInterface) {
-			queue.Queue(func(st render.RenderQueueState) {
-				gl.Buffer(0).Bind(gl.ELEMENT_ARRAY_BUFFER)
-				tex := rendertest.GivenATexture("red/0.png")
-				rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
-			})
-			queue.Purge()
-
-			So(queue, rendertest.ShouldLookLikeFile, "subred")
+		testbuilder.WithExpectation(c, "subred", func(st render.RenderQueueState) {
+			gl.Buffer(0).Bind(gl.ELEMENT_ARRAY_BUFFER)
+			tex := rendertest.GivenATexture("red/0.png")
+			rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
 		})
-		rendertest.DeprecatedWithGlForTest(screen.Dx(), screen.Dy(), func(sys system.System, queue render.RenderQueueInterface) {
-			queue.Queue(func(st render.RenderQueueState) {
-				someStaleBuffer := rendertest.GivenABufferWithData([]float32{
-					77, 55, 44, 33, 22, 11,
-				})
-				someStaleBuffer.Bind(gl.ELEMENT_ARRAY_BUFFER)
-				tex := rendertest.GivenATexture("red/0.png")
-				rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
+		testbuilder.WithExpectation(c, "subred", func(st render.RenderQueueState) {
+			someStaleBuffer := rendertest.GivenABufferWithData([]float32{
+				77, 55, 44, 33, 22, 11,
 			})
-			queue.Purge()
-
-			So(queue, rendertest.ShouldLookLikeFile, "subred")
+			someStaleBuffer.Bind(gl.ELEMENT_ARRAY_BUFFER)
+			tex := rendertest.GivenATexture("red/0.png")
+			rendertest.DrawTexturedQuad(subscreen, tex, st.Shaders())
 		})
-
 	})
 
 	Convey("GivenATexture must panic if there's no image file", t, func(c C) {
