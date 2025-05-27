285576e4c9225f95fedc532c6dae928a3607223e
Thomas McKee <caffeine.storm@gmail.com>
2025-05-27 11:49:22 -0400

    test: a simple image dumper function should be in imgmanip
    tdd: red

---
+++ imgmanip/dump_test.go
@@ -0,0 +1,31 @@
+package imgmanip_test
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"testing"
+
+	"github.com/runningwild/glop/imgmanip"
+)
+
+func withTempFile(tempname string, fn func(*os.File)) {
+	f, err := os.CreateTemp("", tempname)
+	if err != nil {
+		panic(fmt.Errorf("couldn't os.CreateTemp(%q): %w", tempname, err))
+	}
+	defer os.Remove(f.Name())
+
+	fn(f)
+}
+
+func TestDumpImage(t *testing.T) {
+	t.Run("can dump an image to a path", func(t *testing.T) {
+		var genericImage image.Image
+		genericImage = givenAnImage()
+
+		withTempFile("dump.png", func(f *os.File) {
+			imgmanip.MustDumpImage(genericImage, f.Name())
+		})
+	})
+}
