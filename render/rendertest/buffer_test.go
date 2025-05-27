0a549825dc5ae7c84bac7e243d1211e790c638d6
Thomas McKee <caffeine.storm@gmail.com>
2025-05-27 12:43:30 -0400

    clean: prefer testbuilder API for rendertests
    tdd: refactor

--- render/rendertest/buffer_test.go
+++ render/rendertest/buffer_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-gl-legacy/gl"
 	"github.com/runningwild/glop/render/rendertest"
+	"github.com/runningwild/glop/render/rendertest/testbuilder"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,7 +13,7 @@ func TestBuffer(t *testing.T) {
 	t.Run("rendertest.GivenABufferWithData", func(t *testing.T) {
 		t.Run("shouldn't clobber gl.ARRAY_BUFFER", func(t *testing.T) {
 			assert := assert.New(t)
-			rendertest.DeprecatedWithGl(func() {
+			testbuilder.Run(func() {
 				oldbuf := gl.GetInteger(gl.ARRAY_BUFFER_BINDING)
 				buf := rendertest.GivenABufferWithData([]float32{
 					0, 1, 2, 3, 4, 5,
@@ -25,7 +26,7 @@ func TestBuffer(t *testing.T) {
 		})
 		t.Run("shouldn't clobber gl.ELEMENT_ARRAY_BUFFER", func(t *testing.T) {
 			assert := assert.New(t)
-			rendertest.DeprecatedWithGl(func() {
+			testbuilder.Run(func() {
 				oldbuf := gl.GetInteger(gl.ELEMENT_ARRAY_BUFFER_BINDING)
 				buf := rendertest.GivenABufferWithData([]float32{
 					0, 1, 2, 3, 4, 5,
