78d7acac19995dedcff1e7dbf117558c117c7b9d
Thomas McKee <caffeine.storm@gmail.com>
2025-06-04 10:15:14 -0400

    docs: don't forget to warn about RGBA vs. NRGBA
    tdd: refactor

--- imgmanip/dump.go
+++ imgmanip/dump.go
@@ -8,6 +8,8 @@ import (
 )
 
 func MustDumpImage(img image.Image, filePath string) {
+	// TODO(tmckee:43): we should warn if someone passes in not-an-NRGBA because
+	// the encoding could be lossy in that case.
 	f, err := os.Create(filePath)
 	if err != nil {
 		panic(fmt.Errorf("couldn't os.Create(%q): %w", filePath, err))
