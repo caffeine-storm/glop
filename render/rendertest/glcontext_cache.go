23ff28d5902b091248cc86f9c4ca2d95f248cc35
Thomas McKee <caffeine.storm@gmail.com>
2025-06-30 11:14:20 -0400

    clean: improve readability
    tdd: refactor

--- render/rendertest/glcontext_cache.go
+++ render/rendertest/glcontext_cache.go
@@ -8,41 +8,42 @@ import (
 	"github.com/runningwild/glop/system"
 )
 
-func makeTestTemplate(checkInvariants bool, fn func(system.System, system.NativeWindowHandle, render.RenderQueueInterface)) func(width, height int, ctx *glContext) {
-	return func(width, height int, ctx *glContext) {
-		e := ctx.prep(width, height, checkInvariants)
-		if e != nil {
-			// Even on error cases, we shouldn't leak GL state.
-			ee := ctx.clean(InvariantsCheckNo)
-			if ee != nil {
-				panic(fmt.Errorf("after prep-failure: %w, couldn't clean: %w", e, ee))
-			}
-			panic(fmt.Errorf("couldn't prep: %w", e))
-		}
+func doTest(checkInvariants bool, width, height int, fn func(system.System, system.NativeWindowHandle, render.RenderQueueInterface)) {
+	ctx, cleanup := getContextFromCache(width, height)
+	defer cleanup()
 
-		e = ctx.run(fn)
-
-		ee := ctx.clean(checkInvariants)
+	e := ctx.prep(width, height, checkInvariants)
+	if e != nil {
+		// Even on error cases, we shouldn't leak GL state.
+		ee := ctx.clean(InvariantsCheckNo)
 		if ee != nil {
-			panic(fmt.Errorf("couldn't clean: %w, testresult: %w", ee, e))
+			panic(fmt.Errorf("after prep-failure: %w, couldn't clean: %w", e, ee))
+		}
+		panic(fmt.Errorf("couldn't prep: %w", e))
+	}
+
+	e = ctx.run(fn)
+
+	ee := ctx.clean(checkInvariants)
+	if ee != nil {
+		panic(fmt.Errorf("couldn't clean: %w, testresult: %w", ee, e))
+	}
+
+	if e != nil {
+		halting := &conveyIsHalting{}
+		if errors.As(e, &halting) {
+			// It might be that Convey is trying to halt the tests; we need to
+			// preserve their semantics in that case.
+			panic(halting.s)
 		}
 
-		if e != nil {
-			halting := &conveyIsHalting{}
-			if errors.As(e, &halting) {
-				// It might be that Convey is trying to halt the tests; we need to
-				// preserve their semantics in that case.
-				panic(halting.s)
-			}
-
-			panic(fmt.Errorf("error on render-thread: %w", e))
-		}
+		panic(fmt.Errorf("error on render-thread: %w", e))
 	}
 }
 
 var glTestContextSource = make(chan *glContext, 24)
 
-func runOverCachedContext(width, height int, dotest func(int, int, *glContext)) {
+func getContextFromCache(width, height int) (*glContext, func()) {
 	var theContext *glContext
 	select {
 	case cachedContext := <-glTestContextSource:
@@ -50,22 +51,19 @@ func runOverCachedContext(width, height int, dotest func(int, int, *glContext)) 
 	default:
 		theContext = newGlContextForTest(width, height)
 	}
-	defer func() {
+
+	return theContext, func() {
 		if theContext.render.IsDefunct() {
 			return
 		}
 		glTestContextSource <- theContext
-	}()
-
-	dotest(width, height, theContext)
+	}
 }
 
 func RunDeprecatedTestWithCachedContext(width, height int, fn func(system.System, system.NativeWindowHandle, render.RenderQueueInterface)) {
-	dotest := makeTestTemplate(InvariantsCheckNo, fn)
-	runOverCachedContext(width, height, dotest)
+	doTest(InvariantsCheckNo, width, height, fn)
 }
 
 func RunTestWithCachedContext(width, height int, fn func(system.System, system.NativeWindowHandle, render.RenderQueueInterface)) {
-	dotest := makeTestTemplate(InvariantsCheckYes, fn)
-	runOverCachedContext(width, height, dotest)
+	doTest(InvariantsCheckYes, width, height, fn)
 }
