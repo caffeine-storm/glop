d7b8cc1b566adc0242bf922e4a797d5db2976261
Thomas McKee <caffeine.storm@gmail.com>
2025-07-17 11:10:12 -0400

    test: prefer to exercise the negative-press-amount case for wheel event synthesis-testing
    tdd: refactor

--- gui/guitest/events_test.go
+++ gui/guitest/events_test.go
@@ -11,12 +11,12 @@ import (
 func TestSynthesize(t *testing.T) {
 	t.Run("WheelDown", func(t *testing.T) {
 		assert := assert.New(t)
-		synthesized := guitest.SynthesizeEvents().WheelDown(42)
+		synthesized := guitest.SynthesizeEvents().WheelDown(-42)
 
 		mouseWheelKeyId := gin.AnyMouseWheelVertical
 		mouseWheelKeyId.Device.Index = 0
 
 		assert.True(synthesized.IsPressed(mouseWheelKeyId))
-		assert.Equal(float64(42), synthesized.PrimaryEvent().Key.FramePressTotal())
+		assert.Equal(float64(-42), synthesized.PrimaryEvent().Key.FramePressTotal())
 	})
 }
