d9be108dd5b7334667bee12ef37ea42239ce80ea
Thomas McKee <caffeine.storm@gmail.com>
2025-07-17 11:04:58 -0400

    test: verify that synthesizing a mouse wheel event group works
    tdd: red

---
+++ gui/guitest/events_test.go
@@ -0,0 +1,21 @@
+package guitest_test
+
+import (
+	"testing"
+
+	"github.com/runningwild/glop/gin"
+	"github.com/runningwild/glop/gui/guitest"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSynthesize(t *testing.T) {
+	t.Run("WheelDown", func(t *testing.T) {
+		assert := assert.New(t)
+		synthesized := guitest.SynthesizeEvents().WheelDown(42)
+
+		mouseWheelKeyId := gin.AnyMouseWheelVertical
+		mouseWheelKeyId.Device.Index = 0
+
+		assert.True(synthesized.IsPressed(mouseWheelKeyId))
+	})
+}
