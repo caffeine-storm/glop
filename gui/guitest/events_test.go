6f6fa5b8cba572a89a6d9c355b640718fc81f783
Thomas McKee <caffeine.storm@gmail.com>
2025-07-23 15:07:49 -0400

    test: make sure we can synthesize a mouse drag sequence of events
    tdd: red

--- gui/guitest/events_test.go
+++ gui/guitest/events_test.go
@@ -1,12 +1,36 @@
 package guitest_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/runningwild/glop/gin"
+	"github.com/runningwild/glop/gin/aggregator"
+	"github.com/runningwild/glop/gui"
 	"github.com/runningwild/glop/gui/guitest"
 	"github.com/stretchr/testify/assert"
 )
+
+func findEvent(events []gui.EventGroup, pred func(gui.EventGroup) bool) gui.EventGroup {
+	idx := -1
+	for i := range events {
+		if !pred(events[i]) {
+			continue
+		}
+
+		if idx != -1 {
+			panic(fmt.Errorf("more than one event matched the predicate: %d and %d", idx, i))
+		}
+
+		idx = i
+	}
+
+	if idx == -1 {
+		panic(fmt.Errorf("no event matched the predicate"))
+	}
+
+	return events[idx]
+}
 
 func TestSynthesize(t *testing.T) {
 	t.Run("WheelDown", func(t *testing.T) {
@@ -18,5 +42,29 @@ func TestSynthesize(t *testing.T) {
 
 		assert.True(synthesized.IsPressed(mouseWheelKeyId))
 		assert.Equal(float64(-42), synthesized.PrimaryEvent().Key.FramePressTotal())
+	})
+
+	t.Run("dragging", func(t *testing.T) {
+		assert := assert.New(t)
+		fromPos := gui.Point{
+			X: 4, Y: 4,
+		}
+		toPos := gui.Point{
+			X: 7, Y: 42,
+		}
+
+		synthesized := guitest.SynthesizeEvents().MouseDrag(fromPos, toPos)
+
+		numEvents := len(synthesized)
+		assert.Greater(numEvents, 0, "there should be some events")
+
+		mouseDown := findEvent(synthesized, func(ev gui.EventGroup) bool {
+			return ev.PrimaryEvent().Type == aggregator.Press
+		})
+		assert.Equal(mouseDown.GetMousePosition(), fromPos)
+		mouseUp := findEvent(synthesized, func(ev gui.EventGroup) bool {
+			return ev.PrimaryEvent().IsRelease()
+		})
+		assert.Equal(mouseUp.GetMousePosition(), toPos)
 	})
 }
