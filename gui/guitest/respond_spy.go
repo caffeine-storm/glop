409f3cbe0954d1ada8fd9ef9c815ea883f917e35
Thomas McKee <caffeine.storm@gmail.com>
2025-09-11 14:19:25 -0400

    refactor: tweak synth-event responder to match gui widgets' Respond
    tdd: refactor

--- gui/guitest/respond_spy.go
+++ gui/guitest/respond_spy.go
@@ -3,15 +3,16 @@ package guitest
 import "github.com/runningwild/glop/gui"
 
 type Responder interface {
-	Respond(gui *gui.Gui, evt gui.EventGroup)
+	Respond(gui *gui.Gui, evt gui.EventGroup) bool
 }
 
 type RespondSpy struct {
 	events []gui.EventGroup
 }
 
-func (spy *RespondSpy) Respond(gui *gui.Gui, evt gui.EventGroup) {
+func (spy *RespondSpy) Respond(gui *gui.Gui, evt gui.EventGroup) bool {
 	spy.events = append(spy.events, evt)
+	return false
 }
 
 func (spy *RespondSpy) GetEvents() []gui.EventGroup {
