fb679d97988255f29772c7c32da5d4e037640fdc
Thomas McKee <caffeine.storm@gmail.com>
2025-10-18 09:34:44 -0400

    test: gui.MakeRegion should be a thing
    tdd: red

--- gui/geometry_test.go
+++ gui/geometry_test.go
@@ -7,29 +7,43 @@ import (
 	"github.com/runningwild/glop/render/rendertest"
 	"github.com/runningwild/glop/render/rendertest/testbuilder"
 	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestRegionClipping(t *testing.T) {
-	Convey("region clipping", t, func(c C) {
-		testbuilder.New().WithExpectation(c, "red-with-border").Run(func() {
-			// Set a clipping region to block any drawing outside of a square in the
-			// middle.
-			r := gui.Region{
-				Point: gui.Point{
-					X: 4,
-					Y: 4,
-				},
-				Dims: gui.Dims{
-					Dx: 56,
-					Dy: 56,
-				},
-			}
+func TestRegion(t *testing.T) {
+	t.Run("clipping", func(t *testing.T) {
+		Convey("region clipping", t, func(c C) {
+			testbuilder.New().WithExpectation(c, "red-with-border").Run(func() {
+				// Set a clipping region to block any drawing outside of a square in the
+				// middle.
+				r := gui.Region{
+					Point: gui.Point{
+						X: 4,
+						Y: 4,
+					},
+					Dims: gui.Dims{
+						Dx: 56,
+						Dy: 56,
+					},
+				}
 
-			r.PushClipPlanes()
-			defer r.PopClipPlanes()
+				r.PushClipPlanes()
+				defer r.PopClipPlanes()
 
-			// Draw a red square across the 'whole' viewport.
-			rendertest.DrawRectNdc(-1, -1, 1, 1)
+				// Draw a red square across the 'whole' viewport.
+				rendertest.DrawRectNdc(-1, -1, 1, 1)
+			})
 		})
+	})
+
+	t.Run("MakeRegion", func(t *testing.T) {
+		assert := assert.New(t)
+		x, y, dx, dy := 4, 8, 6, 2
+		region := gui.MakeRegion(x, y, dx, dy)
+
+		assert.Equal(region.X, x)
+		assert.Equal(region.Y, y)
+		assert.Equal(region.Dx, dx)
+		assert.Equal(region.Dy, dy)
 	})
 }
